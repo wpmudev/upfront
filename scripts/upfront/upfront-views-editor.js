7128e8fe53c4f2dd48c1f8825cea8dab5941ff50
javier <javier@f53fe46e-8c02-459b-9fdd-93e75771b278>
2013-06-21 17:59:12 +0000

    Created the Upfront notification system.
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/themes/upfront@9538 f53fe46e-8c02-459b-9fdd-93e75771b278

--- scripts/upfront/upfront-views-editor.js
+++ scripts/upfront/upfront-views-editor.js
@@ -2487,6 +2487,82 @@ define(_template_files, function () {
 		}
 	});
 
+	var NotifierView = Backbone.View.extend({
+		notices: new Backbone.Collection([]),
+		elId: 'upfront-notice',
+		timer: false,
+		timeoutTime: 5000,
+		$notice: false,
+		tpl: _.template($(_Upfront_Templates.popup).find('#upfront-notifier-tpl').html()),
+		initialize: function(options){
+			this.notices.on('add', this.messageAdded, this);
+			this.notices.on('remove', this.messageRemoved, this);
+
+			$('body').append(this.tpl({}));
+
+			this.setElement($('#' + this.elId));
+
+			// Hey admin bar!
+			if($('#wpadminbar').length)
+				$('#upfront-notifier').css({top: 28});
+		},
+		addMessage: function(message, type){
+			var notice = {
+				message: message ? message : 'No message', 
+				type: type ? type : 'info'
+			};
+			
+			this.notices.add(notice);
+		},
+		show: function(notice) {
+			var me = this;
+			this.setMessage(notice);
+			this.$el.addClass('notify open')
+				.removeClass('out')
+			;
+			this.timer = setTimeout(function(){
+				me.notices.remove(notice);
+			}, this.timeoutTime)
+		},
+		replace: function(notice) {
+			var me = this;
+			this.setMessage(notice);
+			this.timer = setTimeout(function(){
+				me.notices.remove(notice);
+			}, this.timeoutTime);
+
+			this.$el.removeClass('notify').
+				addClass('shake');
+
+			setTimeout(function(){
+				me.$el.removeClass('shake');
+			}, this.timeoutTime / 2);
+		},
+		setMessage: function(notice) {
+			this.$el.removeClass('info warning error')
+				.addClass(notice.get('type'))
+				.html(notice.get('message'))
+			;
+		},
+		close: function() {
+			this.$el.addClass('out');
+			this.$el.removeClass('notify shake open');
+		},
+		messageAdded: function(notice){
+			if(! this.$el.hasClass('notify')){
+				this.show(notice);
+			}
+		},
+		messageRemoved: function(notice){
+			if(this.notices.length)
+				this.replace(this.notices.at(0));
+			else
+				this.close();
+		}
+	});
+
+	var notifier = new NotifierView();
+
 	return {
 		"Editor": {
 			"Property": Property,
@@ -2504,6 +2580,9 @@ define(_template_files, function () {
 				"Sidebar": Sidebar,
 				"Panel": SidebarPanel,
 				"Element": DraggableElement
+			},
+			notify : function(message, type){
+				notifier.addMessage(message, type);
 			}
 		},	
 		"ContentEditor": {
