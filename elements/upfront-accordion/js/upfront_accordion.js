7a51592fe8c24e6fcffc078b442edbc9322c2e9c
furqan <furqan@f53fe46e-8c02-459b-9fdd-93e75771b278>
2013-08-28 22:47:21 +0000

    Now we can add and remove panels.
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/themes/upfront@10117 f53fe46e-8c02-459b-9fdd-93e75771b278

--- elements/upfront-accordion/js/upfront_accordion.js
+++ elements/upfront-accordion/js/upfront_accordion.js
@@ -15,11 +15,71 @@
     var AccordionView = Upfront.Views.ObjectView.extend({
         events: {
             'editor-change [contenteditable]': 'saveContent',
-            'click .ui-accordion-header'  : 'EditTabs'
+            'click .ui-accordion-header'  : 'edit_panels',
+            'click .add_new_panel'  : 'add_new_panel',
+            'click .remove_panel'  : 'delete_panel',
+            'hover .accordion h3'  : 'hover_panel',
+            'hover .remove_panel'  : 'show_this'
         },
-        EditTabs: function(e){
+        edit_panels: function(e){
             $(e.target).parent().find('div.ui-accordion-content').not($(e.target).next('div.ui-accordion-content')).slideUp();
             $(e.target).next('div.ui-accordion-content').slideDown();
+        },
+        delete_panel: function(e){
+            e.preventDefault();
+            var id = parseInt($(e.target).attr('data-id'), 10),
+                 headers = this.$el.find('.accordion h3');
+            $(headers[id]).next('div').remove().end().remove();
+            $(e.target).remove();
+            this.$el.find('.accordion').accordion('destroy').accordion();
+            this.append_removes();
+        },
+        add_new_panel: function(e){
+            e.preventDefault();
+            var index = this.$el.find('.accordion h3').length + 1;
+            this.$el.find('.accordion').accordion();
+            var new_panel = '<h3>Section '+index+'</h3><div><p>My awesome stub content goes here</p></div>'
+            this.$el.find('.accordion').append(new_panel).accordion('destroy').accordion();
+            this.$el.find('.upfront-object-content').append('<a href="#" data-id="'+(index-1)+'" class="remove_panel">x Remove</a>');
+        },
+        show_this: function(e){
+            if (e.type == "mouseenter") {
+                $(e.target).show()
+            }
+            else {
+                $(e.target).hide();
+            }
+        },
+        hover_panel: function(e){
+            this.set_delete_button_position();
+            var myString = e.target.id,
+                parts = myString.split("-"),
+                thePart = parts[4],
+                id = parseInt(thePart, 10),
+                remove = this.$el.find('.remove_panel[data-id="'+id+'"]');
+            if (e.type == "mouseenter") {
+                remove.show();
+            }
+            else {
+                remove.hide();
+            }
+            this.set_delete_button_position();
+        },
+        set_delete_button_position: function(){
+            var me = this,
+                removes = this.$el.find('.remove_panel');
+            if(!removes.length)
+                return false;
+            removes.each(function(){
+                var id = parseInt($(this).attr('data-id'), 10),
+                    headers = me.$el.find('.accordion h3'),
+                    position, top, left, width;
+                position = $(headers[id]).position(),
+                width = $(headers[id]).width(),
+                top = position.top,
+                left = position.left + (width - 70);
+                $(this).css({'top': top, 'left': left});
+            });
         },
         get_content_markup: function () {
             return ['<div class="upfront-accordion" contenteditable="true">',
@@ -29,6 +89,14 @@
             this.model.set_content($(event.currentTarget).html(), {silent: true});
             event.stopPropagation(); // Only this view handles 'editor-change' of descendant contenteditables.
         },
+        append_removes: function(){
+            this.$el.find('.upfront-object-content .remove_panel').remove();
+            var index = this.$el.find('.accordion h3').length;
+            for (var i=0; i<index; i++)
+            {
+                this.$el.find('.upfront-object-content').append('<a href="#" data-id="'+i+'" class="remove_panel">x Remove</a>');
+            }
+        },
         on_render: function(){
             var me = this;
             setTimeout(
@@ -37,6 +105,8 @@
                         heightStyle: "content"
                     });
                 }, 50);
+            this.append_removes();
+            this.$el.append('<a href="#" class="add_new_panel">+ Add New Panel</a>');
         }
     });
 
