d9b0e0bba42613dedb3bb9d9ec5872f81e51fc75
vladislav <vladislav@f53fe46e-8c02-459b-9fdd-93e75771b278>
2013-09-06 04:14:26 +0000

    No more font awesome.
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/themes/upfront@10345 f53fe46e-8c02-459b-9fdd-93e75771b278

--- functions.php
+++ functions.php
@@ -149,7 +149,7 @@ EOAdminStyle;
 		//wp_enqueue_media();
 		
 		// Enqueue needed styles
-		wp_enqueue_style('font-awesome', self::get_root_url() . '/styles/font-awesome.min.css');
+		//wp_enqueue_style('font-awesome', self::get_root_url() . '/styles/font-awesome.min.css'); // No mo font awesome
 		wp_enqueue_style('upfront-global', self::get_root_url() . '/styles/global.css');
 		wp_enqueue_style('upfront-editor-grid', admin_url('admin-ajax.php?action=upfront_load_editor_grid'));
 		wp_enqueue_style('upfront-editor-interface', self::get_root_url() . '/styles/editor-interface.css');
