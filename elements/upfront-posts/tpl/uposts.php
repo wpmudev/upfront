a69c71b1c8c8b7a4376868945b3729d9fec24650
vladislav <vladislav@f53fe46e-8c02-459b-9fdd-93e75771b278>
2013-09-09 04:43:15 +0000

    Do *not* simply append random post ID classes to uposts elements, all hell breaks loose.
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/themes/upfront@10395 f53fe46e-8c02-459b-9fdd-93e75771b278

--- elements/upfront-posts/tpl/uposts.php
+++ elements/upfront-posts/tpl/uposts.php
@@ -4,7 +4,7 @@
 <ul class='uposts-posts'>
 <?php while(have_posts()): the_post(); 
  	global $post;
-	$classes .= 'uposts-post uposts-posts-' . get_the_ID() . ' ';
+	$classes = 'uposts-post uposts-posts-' . get_the_ID() . ' ';
 
  ?>
 	<li <?php apply_filters('upfront_posts_post_classes', post_class($classes), $post) ?> data-post_id="<?php the_ID() ?>">
