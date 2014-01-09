18a61e0cf36062094cb67b7dbd6c38aeed64d53a
javier <javier@f53fe46e-8c02-459b-9fdd-93e75771b278>
2014-01-09 15:46:43 +0000

    Refactored the default slider jquery plugin to allow to call its methods from outside.
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/themes/upfront@12424 f53fe46e-8c02-459b-9fdd-93e75771b278

--- scripts/default-slider.js
+++ scripts/default-slider.js
@@ -1,6 +1,39 @@
 (function( $ ) {
 	$.fn.upfront_default_slider = function (args) {
-		var data = {
+		var isMethod = typeof args === 'string',
+			result
+		;
+
+		this.each(function(){
+			var $slider = $(this),
+				uslider = $slider.data('uslider')
+			;
+
+			if(uslider){
+				if(isMethod)
+					result = uslider.callMethod(args);
+				else
+					$.error('jQuery-Uslider is already instantiated');
+			}
+			else{
+				if(isMethod)
+					$.error('Can\'t call the ueditor method ' + args + '. Ueditor not initialized');
+				else { 
+					// Initialize slider					
+					$slider.data('uslider', new JQueryUslider($slider, args));
+				}
+			}
+		});
+
+		if( this.length == 1 & typeof result != 'undefined')
+			return result;
+		return this;
+	};
+
+
+	var JQueryUslider = function($slider, args){
+		var me = this,
+			data = $.extend({
 				auto: true,
 				interval: 5000, // in ms
 				auto_height: true, // true | false
@@ -19,207 +52,257 @@
 					next: 'upfront-default-slider-nav-next'
 				},
 				adjust_slide_size: true,
-				starting_slide: 0
-			},
-			me = this;
-		$.extend(data, args);
-		this.each(function(){
-			var $slider = $(this),
-				$items = data.item ? $slider.find('>'+data.item) : $slider.find('>.'+data.classname.item),
-				$slider_wrap = $('<div class="' + data.classname.slider_wrap + '" />'), 
-				$nav = $('<div class="' + data.classname.nav + '" />'), 
-				$prev = $('<div class="' + data.classname.prev + '" />'), 
-				$next = $('<div class="' + data.classname.next + '" />'),
-				max_height = 0,
-				slider_index = 0,
-				slider_pause = false,
-				slider_auto, slider_interval, slider_effect, slider_control, slider_show_control, slider_t;
-			if ( $slider.data('slider-applied') )
-				return;
-			$slider.data('slider-applied', true);
-			$slider.addClass(data.classname.slider);
-			$slider.append($slider_wrap);
-			
-			// Config
-			update_configs();
-			function update_configs () {
-				$slider.removeClass(data.classname.slider + '-control-' + slider_control);
-				$slider.removeClass(data.classname.slider + '-control-' + slider_show_control);
-				slider_auto = $slider.attr('data-slider-auto');
-				if ( typeof slider_auto != 'string' )
-					slider_auto = data.auto;
-				else
-					slider_auto = slider_auto == '0' ? false : true;
-				slider_interval = $slider.attr('data-slider-interval') || data.interval;
-				slider_effect = $slider.attr('data-slider-effect') || data.effect;
-				slider_control = $slider.attr('data-slider-control') || data.control;
-				slider_show_control = $slider.attr('data-slider-show-control') || data.show_control;
-				$slider.addClass(data.classname.slider + '-control-' + slider_control);
-				$slider.addClass(data.classname.slider + '-control-' + slider_show_control);
+				starting_slide: 0			
+			}, args),
+			$items = data.item ? $slider.find('>'+data.item) : $slider.find('>.'+data.classname.item)
+		;
+
+		this.$slider = $slider.data('slider-applied', true)
+			.addClass(data.classname.slider)
+			.append('<div class="' + data.classname.slider_wrap + '" />')
+		;
+
+		this.opts = data;
+		this.index = 0;
+		this.pause = false;
+		this.timer = false;
+
+		this.update_configs();
+		this.update_items($items);
+
+		this.$slider.append($('<div class="' + data.classname.nav + '" />'));
+		this.update_nav();
+
+		// Next and previous navigation
+		if ( data.control_next_prev )
+			this.prev_next_navigation();
+
+		this.slider_switch(data.starting_slide);
+		this.update_auto_slide();
+
+		this.bind_events();
+	};
+
+	JQueryUslider.prototype = {
+		callMethod: function(method){
+			switch(method) {
+				case 'next':
+					this.next();
+					break;
+				case 'prev':
+					this.prev();
+					break;
 			}
-			
-			// Add items
-			update_items($items);
-			function update_items($new_items){
-				$items = $new_items;
-				$slider_wrap.html('');
-				$slider_wrap.append($items);
-				$items.addClass(data.classname.item);
-				if ( data.auto_height ){ // Auto height adjustment to the highest slide
-					calc_height();
-					$slider.find('img').one('load', calc_height);
-				}
-				else if(data.adjust_slide_size){ // Adjust slides to available space
-					adjust_slide_size();
-				}
+		},
+
+		update_configs: function(){
+			var $slider = this.$slider,			
+				slider_auto = $slider.attr('data-slider-auto'),
+				data = this.opts
+			;
+			$slider.removeClass(data.classname.slider + '-control-' + data.control);
+			$slider.removeClass(data.classname.slider + '-control-' + data.show_control);
+			if ( typeof slider_auto != 'string' )
+				slider_auto = data.auto;
+			else
+				slider_auto = slider_auto == '0' ? false : true;
+			data.auto = slider_auto;
+			data.interval = $slider.attr('data-slider-interval') || data.interval;
+			data.effect = $slider.attr('data-slider-effect') || data.effect;
+			data.control = $slider.attr('data-slider-control') || data.control;
+			data.show_control = $slider.attr('data-slider-show-control') || data.show_control;
+			$slider.addClass(data.classname.slider + '-control-' + data.control);
+			$slider.addClass(data.classname.slider + '-control-' + data.show_control);			
+		},
+
+		update_items: function($new_items){
+			var $slider = this.$slider,
+				data = this.opts,
+				$slider_wrap = $slider.find('.' + data.classname.slider_wrap)
+			;
+			this.items = $new_items;
+
+			$slider_wrap.html('')
+				.append(this.items)
+			;
+
+			this.items.addClass(data.classname.item);
+			if ( data.auto_height ){ // Auto height adjustment to the highest slide
+				this.calc_height();
+				$slider.find('img').one('load', $.proxy(this.calc_height, this));
 			}
-			
-			function calc_height () {
-				$slider.css('height', 9999);
-				$items.each(function(index){
-					var $img = $(this).find('img'),
-						img_h = $img.height();
-					max_height = max_height > img_h ? max_height : img_h;
-				});
-				$slider.css('height', Math.ceil(max_height/15)*15);
-			}
-			
-			function adjust_slide_size () {
-				var height = $slider.outerHeight(),
-					width = $slider.outerWidth();
-				$items.each(function(){
-					var $img = $(this).find('img'),
-						img_h = $img.height(),
-						img_w = $img.width();
-					if ( height/width > img_h/img_w )
-						$img.css({ height: '100%', width: 'auto' });
-					else
-						$img.css({ height: 'auto', width: '100%' });
-				});
-			}
-			
-			if ( !data.auto_height && data.adjust_slide_size){
-				$(window).on('load', adjust_slide_size);
-				$(window).on('resize', adjust_slide_size);
-			}
-					
-			// Add navigation
-			update_nav();
-			$slider.append($nav);
-			function update_nav () {
-				$nav.html('');
-				if ( data.control_num ){
-					$items.each(function(index){
-						$nav.append('<i class="' + data.classname.nav_item + '" data-slider-index="' + index + '">'+index+'</i>');
-					});
-					$slider.on('click', '.'+data.classname.nav_item, function(e){
-						e.preventDefault();
-						var index = $(this).data('slider-index');
-						slider_switch(index);
-						slider_pause = true;
-					});
-				}
-			}
-			
-			// Next and previous navigation
-			if ( data.control_next_prev ){
-				$slider.append($prev);
-				$slider.on('click', '.'+data.classname.prev, function(e){
-					e.preventDefault();
-					var fx = ( slider_effect == 'slide-left' || slider_effect == 'slide-right') ? 'slide-right' : ( slider_effect == 'crossfade' ? 'crossfade' : 'slide-down' );
-					slider_switch(slider_index > 0 ? slider_index-1 : $items.length-1, false, fx);
-					slider_pause = true;
-				});
-				$slider.append($next);
-				$slider.on('click', '.'+data.classname.next, function(e){
-					e.preventDefault();
-					var fx = ( slider_effect == 'slide-left' || slider_effect == 'slide-right') ? 'slide-left' : ( slider_effect == 'crossfade' ? 'crossfade' : 'slide-up' );
-					slider_switch(slider_index+1 >= $items.length ? 0 : slider_index+1, false, fx);
-					slider_pause = true;
-				});
-			}
-			function slider_switch (index, is_auto, effect) {
-				var $n = $nav.find('.'+data.classname.nav_item).eq(index),
-					$item = $items.eq(index),
-					effect = effect ? effect : slider_effect,
-					current = $slider.find('.'+data.classname.item+'-current')
+			else if(data.adjust_slide_size){ // Adjust slides to available space
+				this.adjust_slide_size();
+			}			
+		},
+
+		calc_height: function(){
+			var max_height;
+			this.$slider.css('height', 9999);
+			this.items.each(function(index){
+				var $img = $(this).find('img'),
+					img_h = $img.height()
 				;
-				if ( !$item.hasClass(data.classname.item+'-current') && !(is_auto && slider_pause) ){
-					$slider.trigger('slideout', current);
-					current.removeClass(data.classname.item+'-current');
-					$item.addClass(data.classname.item+'-current');
-					$nav.find('.'+data.classname.nav_item+'-selected').removeClass(data.classname.nav_item+'-selected');
-					$n.addClass(data.classname.nav_item+'-selected');
-					slider_index = index;
-					// Animation effect
-					$item.addClass(data.classname.item+'-effect-'+effect);
-					$item.one('animationend webkitAnimationEnd MSAnimationEnd oAnimationEnd webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function () {
-						$(this).removeClass(data.classname.item+'-effect-'+effect);
-						if($item.hasClass(data.classname.item+'-current'))
-							$slider.trigger('slidein', [$item, index]);
-					});
-				}
-				slider_pause = false;
+				max_height = max_height > img_h ? max_height : img_h;
+			});
+			this.$slider.css('height', Math.ceil(max_height/15)*15);			
+		},
+
+		adjust_slide_size: function(){			
+			var height = this.$slider.outerHeight(),
+				width = this.$slider.outerWidth()
+			;
+
+			this.items.each(function(){
+				var $img = $(this).find('img'),
+					img_h = $img.height(),
+					img_w = $img.width()
+				;
+				if ( height/width > img_h/img_w )
+					$img.css({ height: '100%', width: 'auto' });
+				else
+					$img.css({ height: 'auto', width: '100%' });
+			});
+		},
+
+		update_nav: function(){
+			var me = this,
+				data = this.opts,
+				$nav = this.$slider.find(data.classname.nav)
+			;
+			$nav.html('');
+			if ( data.control_num ){
+				this.items.each(function(index){
+					$nav.append('<i class="' + data.classname.nav_item + '" data-slider-index="' + index + '">'+index+'</i>');
+				});
+				this.$slider.on('click', '.'+data.classname.nav_item, function(e){
+					e.preventDefault();
+					var index = $(this).data('slider-index');
+					me.slider_switch(index);
+					me.pause = true;
+				});
+			}			
+		},
+
+		prev_next_navigation: function(){
+			var me = this,
+				data = this.opts
+			;
+			this.$slider.append('<div class="' + data.classname.prev + '" /><div class="' + data.classname.next + '" />')
+				.on('click', '.'+data.classname.prev, function(e){
+					e.preventDefault();
+					me.prev();
+				})
+				.on('click', '.'+data.classname.next, function(e){
+					e.preventDefault();
+					me.next();
+				})
+			;			
+		},
+
+		next: function(){
+			var data = this.opts,
+				fx = ( data.effect == 'slide-left' || data.effect == 'slide-right') ? 'slide-left' : ( data.effect == 'crossfade' ? 'crossfade' : 'slide-up' )
+			;
+			this.slider_switch(this.index+1 >= this.items.length ? 0 : this.index+1, false, fx);
+			this.pause = true;
+		},
+
+		prev: function(){
+			var data = this.opts,
+				fx = ( data.effect == 'slide-left' || data.effect == 'slide-right') ? 'slide-right' : ( data.effect == 'crossfade' ? 'crossfade' : 'slide-down' )
+			;
+			this.slider_switch(this.index > 0 ? this.index-1 : this.items.length-1, false, fx);
+			this.pause = true;
+		},
+
+		slider_switch: function(index, is_auto, effect){
+			var data = this.opts,
+				$nav = this.$slider.find(data.classname.nav),
+				$slider = this.$slider,
+				$n = $nav.find('.'+data.classname.nav_item).eq(index),
+				$item = this.items.eq(index),
+				current = $slider.find('.'+data.classname.item+'-current')
+			;
+			if(!effect)
+				effect = data.effect;
+
+			if ( !$item.hasClass(data.classname.item+'-current') && !(is_auto && this.pause) ){
+				$slider.trigger('slideout', current);
+				current.removeClass(data.classname.item+'-current');
+				$item.addClass(data.classname.item+'-current');
+				$nav.find('.'+data.classname.nav_item+'-selected').removeClass(data.classname.nav_item+'-selected');
+				$n.addClass(data.classname.nav_item+'-selected');
+				this.index = index;
+				// Animation effect
+				$item.addClass(data.classname.item+'-effect-'+effect);
+				$item.one('animationend webkitAnimationEnd MSAnimationEnd oAnimationEnd webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function () {
+					$(this).removeClass(data.classname.item+'-effect-'+effect);
+					if($item.hasClass(data.classname.item+'-current'))
+						$slider.trigger('slidein', [$item, index]);
+				});
 			}
-			slider_switch(data.starting_slide);
-			
-			update_auto_slide();
-			function update_auto_slide () {
-				if ( slider_t )
-					clearInterval(slider_t);
-				if ( slider_auto && slider_interval > 999 ){
-					slider_t = setInterval(function(){
-						slider_switch( slider_index+1 >= $items.length ? 0 : slider_index+1, true );
-					}, slider_interval);
-				}
-			}
-			
-			// Refresh this slider
-			me.on('refresh', function(){
+			this.pause = false;			
+		},
+
+		update_auto_slide: function() {
+			var me = this,
+				data = this.opts
+			;
+			if ( this.timer )
+				clearInterval(this.timer);
+			if ( data.auto && data.interval > 999 ){
+				this.timer = setInterval(function(){
+					me.slider_switch( me.index+1 >= me.items.length ? 0 : me.index+1, true );
+				}, data.interval);
+			}			
+		},
+
+		bind_events: function(){
+			var me = this,
+				$slider = me.$slider,
+				data = me.opts
+			;	
+			$slider.on('refresh', function(){
 				var $new_items = data.item ? $slider.find('>'+data.item) : $slider.find('>.'+data.classname.item),
 					new_length = $new_items.length,
-					length = $items.length;
-				update_configs();
-				update_auto_slide();
+					length = me.items.length
+				;
+				me.update_configs();
+				me.update_auto_slide();
 				if ( new_length ){
-					update_items($new_items);
-					update_nav();
+					me.update_items($new_items);
+					me.update_nav();
 					if ( new_length != length )
-						slider_switch(0);
+						me.slider_switch(data.starting_slide);
 				}
-				if ( !data.auto_height &&  data.adjust_slide_size){
-					adjust_slide_size();
-					$items.find('img').one('load', adjust_slide_size);
+				if ( !data.auto_height && data.adjust_slide_size){
+					me.adjust_slide_size();
+					me.items.find('img').one('load', $.proxy(me.adjust_slide_size, me));
 				}
-			});
-			
-			// Pause slider
-			me.on('pause', function(){
-				clearInterval(slider_t);
-			});
-			
-			// Resume
-			me.on('resume', function(){
-				update_auto_slide();
-			});
-		});
-	};
-	
+			})
+			.on('pause', function(){
+				clearInterval(me.timer);
+			})
+			.on('resume', function(){
+				me.update_auto_slide();
+			});			
+		}
+	};	
 	
 	$(document).ready(function(){
-		
 		// Inline post slider setup
 		var inline_slider = {
 			item: '.upfront-inserted_image-wrapper',
 			control_next_prev: false
-		}
+		};
 		$('.upfront-inline_post-slider').upfront_default_slider(inline_slider);
 		
 		// Bg slider
 		var bg_slider = {
 			auto_height: false,
 			control: 'inside'
-		}
+		};
 		$('.upfront-bg-slider').upfront_default_slider(bg_slider);
 		
 		// Integration with Upfront editor
@@ -235,18 +318,19 @@
 					else
 						$s.upfront_default_slider(bg_slider);
 				}
-			})/*,
-			CKEDITOR.on('currentInstance', function(){
-				editor = CKEDITOR.currentInstance;
-				//$('.upfront-inline_post-slider').upfront_default_slider(inline_slider);
-				if ( !editor )
-					return;
-				editor.on('insertHtml', function(){
-				//	$('.upfront-inline_post-slider').upfront_default_slider(inline_slider);
-				});
-			}); */
+			});
 		});
-		
+
+		/*
+		CKEDITOR.on('currentInstance', function(){
+			editor = CKEDITOR.currentInstance;
+			//$('.upfront-inline_post-slider').upfront_default_slider(inline_slider);
+			if ( !editor )
+				return;
+			editor.on('insertHtml', function(){
+			//	$('.upfront-inline_post-slider').upfront_default_slider(inline_slider);
+			});
+		}); */		
 	});
  
 }( jQuery ));
