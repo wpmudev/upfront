7e00507a2565b955c0bf0ca0f41ce9a7cb10ad19
vladislav <vladislav@f53fe46e-8c02-459b-9fdd-93e75771b278>
2014-02-14 06:18:30 +0000

    Implementing Ace syntax editor.
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/themes/upfront@12892 f53fe46e-8c02-459b-9fdd-93e75771b278

--- elements/upfront-code/js/upfront_code.js
+++ elements/upfront-code/js/upfront_code.js
@@ -22,13 +22,21 @@ var Views = {
 			"click .create": "create_code"
 		},
 		render: function () {
-			this.$el.empty()
-				.append(
-					'<p style="text-align:center"><button type="button" class="embed">Embed 3rd Party code</button>' +
-					'&nbsp;or&nbsp;' +
-					'<button type="button" class="create">Write Custom Code</button></p>'
-				)
-			;
+			var me = this;
+
+			this.$el.empty().append('Loading...');
+
+			require([
+				'//cdnjs.cloudflare.com/ajax/libs/ace/1.1.01/ace.js'
+			], function () {
+				me.$el.empty()
+					.append(
+						'<p style="text-align:center"><button type="button" class="embed">Embed 3rd Party code</button>' +
+						'&nbsp;or&nbsp;' +
+						'<button type="button" class="create">Write Custom Code</button></p>'
+					)
+				;
+			});
 		},
 		embed_code: function () {
 			this.model.set_property("code_selection_type", "Embed", true);
@@ -43,6 +51,15 @@ var Views = {
 	Create: Upfront.Views.ObjectView.extend({
 		
 		is_editing: false,
+		script_error: false,
+
+		SYNTAX_TYPES: {
+			"markup": "html",
+			"style": "css",
+			"script": "javascript"
+		},
+
+		MIN_HEIGHT: 50,
 
 		get_content_markup: function () {
 			var markup = this.model.get_property_value_by_name('markup') || Upfront.data.upfront_code.defaults.fallbacks.markup,
@@ -56,18 +73,23 @@ var Views = {
 				if (!el.length) return true;
 				style += '#' + element_id + ' .upfront_code-element ' + el + '}';
 			});
+
+			// Validate script before adding it in
+			// DOWNSIDE! Executing twice.
+			script = ';try { (function ($) {' + script + "\n" + '})(jQuery); } catch(e) {}';
+			// Set up flag
+			this.script_error = false;
+			try {
+				eval(script);
+			} catch (e) {
+				script = '';
+				this.script_error = true;
+			}
+
 			return '<section class="upfront_code-element clearfix">' + markup +
 				'<style>' + style + '</style>' +
-				'<script>;(function ($) {' + script + "\n" + '})(jQuery);</script>' + '</section>'
-			;
-		},
-
-		update_property: function (model) {
-			var $area = $(this),
-				type = $area.attr("data-type"),
-				value = $area.val()
-			;
-			model.set_property(type, value, true);
+				'<script>' + script + '</script>' +
+			'</section>';
 		},
 
 		on_render: function () {
@@ -82,14 +104,15 @@ var Views = {
 			if (this.is_editing) return false;
 			this.is_editing = true;
 			var me = this,
+				$draggable = this.$el.closest('.ui-draggable'),
 				update_position = function () {
 					var $output = me.$el.find("section.upfront_code-element"),
 						$editor = me.$el.find("section.upfront_code-editor"),
 						top = $output.height() + $output.offset().top
 					;
 					$editor.offset({top: top});
-
-				}
+				},
+				editors = {}
 			;
 			this.$el.append(
 				'<section class="upfront_code-editor upfront_code-editor-complex">' +
@@ -98,20 +121,46 @@ var Views = {
 						'<div class="upfront_code-switch" data-for="style">CSS</div>' +
 						'<div class="upfront_code-switch" data-for="script">JS</div>' +
 						'<div class="upfront_code-editor-section upfront_code-markup active">' +
-							'<textarea data-type="markup" >' + (this.model.get_property_value_by_name('markup') || Upfront.data.upfront_code.defaults.fallbacks.markup)  + '</textarea>' +
+							'<div data-type="markup" >' + (this.model.get_property_value_by_name('markup') || Upfront.data.upfront_code.defaults.fallbacks.markup)  + '</div>' +
 						'</div>' +
 						'<div class="upfront_code-editor-section upfront_code-style">' +
-							'<textarea data-type="style" >' + (this.model.get_property_value_by_name('style') || Upfront.data.upfront_code.defaults.fallbacks.style) + '</textarea>' +
+							'<div data-type="style" >' + (this.model.get_property_value_by_name('style') || Upfront.data.upfront_code.defaults.fallbacks.style) + '</div>' +
 						'</div>' +
 						'<div class="upfront_code-editor-section upfront_code-script">' +
-							'<textarea data-type="script" >' + (this.model.get_property_value_by_name('script') || Upfront.data.upfront_code.defaults.fallbacks.script) + '</textarea>' +
+							'<div data-type="script" >' + (this.model.get_property_value_by_name('script') || Upfront.data.upfront_code.defaults.fallbacks.script) + '</div>' +
 						'</div>' +
+						'<div class="handle ui-resizable-handle ui-resizable-s">&hellip;</div>' +
 					'</div>' +
 					'<button>ok</button>' +
 				'</section>'
 			);
 			update_position();
+			// Disable the draggable
+			$draggable.draggable('disable');
 			this.$el
+				.find(".upfront_code-editor-section>div").each(function () {
+					var $me = $(this),
+						type = $me.attr("data-type") || "markup",
+						code = "markup" === type ? $me.html() : '',
+						editor = ace.edit(this),
+						syntax = me.SYNTAX_TYPES[type],
+					// Height settings
+						$parent = me.$el.closest(".upfront-module"),
+						$out = me.$el.find("section.upfront_code-element"),
+						$button = me.$el.find("section button"),
+						height = ($parent.height() - ($out.outerHeight() + $button.outerHeight())) - 4*Upfront.Settings.LayoutEditor.Grid.baseline
+					;
+					$me.height(
+						(height < me.MIN_HEIGHT ? me.MIN_HEIGHT : height)
+					);
+					editor.getSession().setUseWorker(false);
+					editor.setTheme("ace/theme/monokai");
+					editor.getSession().setMode("ace/mode/" + syntax);
+					if ("markup" === type && code.length) {
+						editor.getSession().setValue(code);
+					}
+					editors[type] = editor;
+				}).end()
 				.find(".upfront_code-switch")
 					.on("click", function () {
 						var $me = $(this),
@@ -120,30 +169,48 @@ var Views = {
 						;
 						$(".upfront_code-switch,.upfront_code-editor-section").removeClass("active");
 						$me.addClass("active");
-						$target.addClass("active").find("textarea").focus();
-
+						$target.addClass("active");
 					})
 				.end()
-				.find("textarea")
-					.on("keyup change", function () {
-						// What about the realtime syntax highlighting?
-						me.update_property.apply(this, [me.model]);
-						me.$el.find("section.upfront_code-element").replaceWith(me.get_content_markup());
-						update_position();
+				.find(".upfront_code-editor-complex-wrapper")
+					.resizable({
+						handles: {
+							s: ".handle"
+						}
 					})
-				.end()
+					.on("resizestop", function (e, ui) {
+						var $editors = me.$el.find(".upfront_code-editor-section>div"),
+							diff = ui.size.height - me.$el.find(".upfront_code-switch").height(),
+							pads = ui.element.outerHeight() - ui.element.height()
+						;
+						$editors.height(diff-pads);
+						_(editors).invoke("resize");
+				}).end()
 				.find("button").on("click", function () {
-					me.$el.find("pre code").each(function () {
-						me.update_property.apply(this, [me.model]);
+					_(editors).each(function (editor, type) {
+						me.model.set_property(type, editor.getValue(), true);
 					});
 					me.$el
 						.find("section.upfront_code-element").replaceWith(me.get_content_markup()).end()
 						.find("section.upfront_code-editor").remove()
 					;
 					me.is_editing = false;
+					$draggable.draggable('enable');
 					me.trigger("code:model:updated");
 				})
 			;
+			_(editors).each(function (editor, type) {
+				editor.on("change", function () {
+					me.model.set_property(type, editor.getValue(), true);
+					me.$el.find("section.upfront_code-element").replaceWith(me.get_content_markup());
+					if (me.script_error) {
+						if (!me.$el.find(".error.javascript").length) me.$el.find(".upfront_code-switch:last").after('<div class="error javascript"><i class="upfront-icon-button" title="There is an error in your JS code"></i></div>');
+					} else {
+						me.$el.find(".error.javascript").remove();
+					}
+					update_position();
+				});
+			});
 		}
 	}),
 
--- elements/upfront-code/lib/upfront_code.php
+++ elements/upfront-code/lib/upfront_code.php
@@ -41,7 +41,7 @@ class Upfront_CodeView extends Upfront_Object {
 
     private function _to_scoped_script ($raw) {
         return !empty($raw)
-            ? "<script>;(function ($) { {$raw}\n })(jQuery);</script>"
+            ? "<script>;try { (function ($) { {$raw}\n })(jQuery); } catch(e) {}</script>"
             : ''
         ;
     }
