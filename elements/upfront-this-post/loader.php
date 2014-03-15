ab0c80d6e062ac15bc9e634695d19816db601523
vladislav <vladislav@f53fe46e-8c02-459b-9fdd-93e75771b278>
2014-03-15 06:32:35 +0000

    Started fixing default post behaviour.
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/themes/upfront@13198 f53fe46e-8c02-459b-9fdd-93e75771b278

--- elements/upfront-this-post/loader.php
+++ elements/upfront-this-post/loader.php
@@ -34,10 +34,13 @@ function this_post_initialize () {
 	require_once (dirname(__FILE__) . '/lib/this_post.php');
 
 	// Add element defaults to data object
-	add_action('upfront_data', array(Upfront_ThisPostView, 'add_js_defaults'));
+	add_action('upfront_data', array('Upfront_ThisPostView', 'add_js_defaults'));
 
 	// Expose our JavaScript definitions to the Upfront API
 	upfront_add_layout_editor_entity('this_post', upfront_relative_element_url('js/this_post', __FILE__));
+
+	// Define fallback selectors, if nothing else got in
+	add_action('upfront_post_selectors', array('Upfront_ThisPostView', 'add_fallback_selectors'), 999); // <-- register late
 
 }
 // Initialize the entity when Upfront is good and ready
