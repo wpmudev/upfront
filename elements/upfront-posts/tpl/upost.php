211fe5bb5fbdf62c756479818c1a7a21b26a25d7
vladislav <vladislav@f53fe46e-8c02-459b-9fdd-93e75771b278>
2014-03-15 07:14:40 +0000

    Allowing for "none" as content type for posts element. Also fixing templates to work with default selectors.
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/themes/upfront@13202 f53fe46e-8c02-459b-9fdd-93e75771b278

--- elements/upfront-posts/tpl/upost.php
+++ elements/upfront-posts/tpl/upost.php
@@ -1,16 +1,15 @@
-<span class='uposts-tumbnail_container'>
-<?php if (!empty($properties['post_data']) && in_array('featured_image', $properties['post_data'])) { ?>
-<div class="entry-thumbnail">
-	 <?php the_post_thumbnail(); ?>
-</div>
-<?php } ?>
+	<span class='uposts-tumbnail_container'>
+	<?php if (!empty($properties['post_data']) && in_array('featured_image', $properties['post_data'])) { ?>
+	<div class="post_thumbnail">
+		 <?php the_post_thumbnail(); ?>
+	</div>
+	<?php } ?>
 </span>
 <h1 class='post_title'><a href='<?php the_permalink() ?>'><?php the_title() ?></a></h1>
-<div class='post_content'>
-<?php 
-	if ($properties['content_type'] == 'excerpt')
-		the_excerpt();
-	else
-		the_content();
-?>
-</div>
+<?php if (!empty($properties['content_type']) && 'excerpt' == $properties['content_type']) { ?>
+	<div class="post_content post_content-excerpt"><?php the_excerpt(); ?></div>
+<?php } else if (!empty($properties['content_type']) && 'none' == $properties['content_type']) { ?>
+	<!-- Nothing here -->
+<?php } else { ?>
+	<div class="post_content post_content-full"><?php the_content(); ?></div>
+<?php } ?>
--- elements/upfront-posts/tpl/uposts.php
+++ elements/upfront-posts/tpl/uposts.php
@@ -6,7 +6,6 @@
 <?php while(have_posts()): the_post(); 
  	global $post, $wp_query;
 	$classes = 'uposts-post uposts-posts-' . get_the_ID() . ' ';
-
  ?>
 	<li <?php apply_filters('upfront_posts_post_classes', post_class($classes), $post) ?> data-post_id="<?php the_ID() ?>">
 		<?php echo upfront_get_template('upost', array('properties' => $properties), dirname(__FILE__) . '/upost.php'); ?>
