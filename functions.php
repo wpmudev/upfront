1d2584254e3fc7feaa16e6fb6fc2cbd4befed154
sam <sam@f53fe46e-8c02-459b-9fdd-93e75771b278>
2014-06-12 06:27:43 +0000

    replacing fontawesome with pictos for font icons of redactor air
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/themes/upfront@14894 f53fe46e-8c02-459b-9fdd-93e75771b278

--- functions.php
+++ functions.php
@@ -167,7 +167,8 @@ EOAdminStyle;
 		//wp_enqueue_media();
 
 		// Enqueue needed styles
-		wp_enqueue_style('font-awesome', self::get_root_url() . '/styles/font-awesome.min.css'); // Just for testing icons in the redactor airbuttons
+//		wp_enqueue_style('font-awesome', self::get_root_url() . '/styles/font-awesome.min.css'); // No more fontawsome
+		wp_enqueue_style('pictos', self::get_root_url() . '/styles/pictos_base64.css');
 		wp_enqueue_style('upfront-editor-grid', admin_url('admin-ajax.php?action=upfront_load_editor_grid'));
 		wp_enqueue_style('upfront-editor-interface', self::get_root_url() . '/styles/editor-interface.css');
 
