b13f7dbb9be53ab3dac080fc742d85a5d0d9e022
gagan <gagan@f53fe46e-8c02-459b-9fdd-93e75771b278>
2014-08-19 06:48:37 +0000

    Fixed the bug with tabs/accordions where all the instances were sharing the same content.
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/themes/upfront@15347 f53fe46e-8c02-459b-9fdd-93e75771b278

--- elements/upfront-accordion/js/uaccordion.js
+++ elements/upfront-accordion/js/uaccordion.js
@@ -7,6 +7,16 @@ define(['text!' + 'elements/upfront-accordion/tpl/uaccordion.html'], function(ac
 	var UaccordionModel = Upfront.Models.ObjectModel.extend({
 		init: function () {
 			var properties = _.clone(Upfront.data.uaccordion.defaults);
+			
+			var defaults = Upfront.data.uaccordion.defaults;
+
+			//copy the default panel data by value, so that the source does not get updated if passed by reference
+
+			properties['accordion'] = [];
+			properties['accordion'][0] = {};
+			properties['accordion'][0]['content'] = _.clone(defaults['accordion'][0]['content']);
+			properties['accordion'][0]['title'] = _.clone(defaults['accordion'][0]['title']);
+		
 			properties.element_id = Upfront.Util.get_unique_id("uaccordion-object");
 			this.init_properties(properties);
 		}
--- elements/upfront-tabs/js/utabs.js
+++ elements/upfront-tabs/js/utabs.js
@@ -6,9 +6,19 @@
 		var UtabsModel = Upfront.Models.ObjectModel.extend({
 			init: function () {
 				var properties = _.clone(Upfront.data.utabs.defaults);
+				var defaults = Upfront.data.utabs.defaults;
 				
-				properties['tabs'] = _.clone(properties.tabs);
+				//copy the default tabs data by value, so that the source does not get updated if passed by reference
 				
+				properties['tabs'] = [];
+				properties['tabs'][0] = {};
+				properties['tabs'][0]['content'] = _.clone(defaults['tabs'][0]['content']);
+				properties['tabs'][0]['title'] = _.clone(defaults['tabs'][0]['title']);
+
+				properties['tabs'][1] = {};
+				properties['tabs'][1]['content'] = _.clone(defaults['tabs'][1]['content']);
+				properties['tabs'][1]['title'] = _.clone(defaults['tabs'][1]['title']);
+
 				properties.element_id = Upfront.Util.get_unique_id("utabs-object");
 				this.init_properties(properties);
 			}
@@ -35,7 +45,7 @@
 				if(! (this.model instanceof UtabsModel)){
 					this.model = new UtabsModel({properties: this.model.get('properties')});
 				}
-				console.log(Upfront.data.utabs.defaults);
+
 				this.events = _.extend({}, this.events, {
 					'click .add-item': 'addTab',
 					'click .tabs-tab': 'onTabClick',
@@ -175,7 +185,7 @@
 			},
 
 			saveTabContent: function() {
-								console.log(Upfront.data.utabs.defaults);
+								
 				var $content = this.$el.find('.tab-content-active'),
 					tabId = $content.attr('id').split('-').pop(),
 					ed = $content.data("ueditor"),
@@ -183,7 +193,7 @@
 				;
 				try { text = ed.getValue(true); } catch (e) { text = $content.html(); }
 var me = this;
-				setTimeout(function() {me.property('tabs')[tabId].content = text; console.log('saved')}, 10000);
+					me.property('tabs')[tabId].content = text; 
 
 			},
 
@@ -263,11 +273,9 @@ var me = this;
 						Upfront.Events.trigger('upfront:element:edit:start', 'text');
 						$(this).focus();
 					 }).on("stop", function () {
-						//console.log($content.parent().parent());
 						id = $content.parent().parent().data('content-id').split('-').pop();
-						me.property('tabs')[id].title = $content.text();
+						me.property('tabs')[id]['title'] = $content.text();
 						Upfront.Events.trigger('upfront:element:edit:stop');
-						//me.render();
 					 }).on("blur", function() {
 						$content.data('ueditor').stop(); 
 					 })
