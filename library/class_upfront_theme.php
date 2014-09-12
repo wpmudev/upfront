4ee0cdb0fcaad1b23f91f367360d0a74b61988c9
vladislav <ve@incsub.com>
2014-09-12 07:14:18 +0200

    Refactoring the slider image importing implementation.

--- library/class_upfront_theme.php
+++ library/class_upfront_theme.php
@@ -1171,6 +1171,43 @@ abstract class Upfront_ChildTheme implements IUpfront_Server {
 	public function add_required_posts ($limit, $thumbnails=array()) {
 		return $this->_insert_posts($limit, $thumbnails);
 	}
+
+	/**
+	 * Called from the implementing theme,
+	 * this method will actually import the background slider images.
+	 */
+	protected function _import_slider_image ($filepath) {
+        $key = $this->get_prefix() . '-slider-images';
+        $images = get_option($key, array());
+        if (!empty($images[$filepath])) return $images[$filepath];
+
+        // else import image
+        $wp_upload_dir = wp_upload_dir();
+        $pfx = !empty($wp_upload_dir['path']) ? trailingslashit($wp_upload_dir['path']) : '';
+        if (!function_exists('wp_generate_attachment_metadata')) require_once(ABSPATH . 'wp-admin/includes/image.php');
+        $filename = basename($filepath);
+        while (file_exists("{$pfx}{$filename}")) {
+            $filename = rand() . $filename;
+        }
+        $full_img_path = get_stylesheet_directory() . DIRECTORY_SEPARATOR . ltrim($filepath, '/');
+        @copy($full_img_path, "{$pfx}{$filename}");
+        $wp_filetype = wp_check_filetype(basename($filename), null);
+        $attachment = array(
+            'guid' => $wp_upload_dir['url'] . '/' . basename($filename),
+            'post_mime_type' => $wp_filetype['type'],
+            'post_title' => preg_replace('/\.[^.]+$/', '', basename($filename)),
+            'post_content' => '',
+            'post_status' => 'inherit'
+        );
+        $attach_id = wp_insert_attachment($attachment, "{$pfx}{$filename}");
+        $attach_data = wp_generate_attachment_metadata( $attach_id, "{$pfx}{$filename}" );
+        wp_update_attachment_metadata( $attach_id, $attach_data );
+
+        $images[$filepath] = $attach_id;
+        update_option($key, $images);
+
+        return $attach_id;
+    }
 }
 
 
