3699dd805bbd98271df874e2d77b50ca71459346
Ivan Ivanić <spoygg@spoygg.com>
2014-11-04 09:22:57 +0100

    Add tabs settings initial presets manager panel

--- elements/upfront-tabs/js/settings.js
+++ elements/upfront-tabs/js/settings.js
@@ -1,12 +1,12 @@
 define([
-	'elements/upfront-tabs/js/settings/appearance-panel'
-], function(AppearancePanel) {
+	'elements/upfront-tabs/js/settings/presets-panel'
+], function(PresetsPanel) {
 	var Settings = Upfront.Views.Editor.Settings.Settings.extend({
 		initialize: function (opts) {
 			this.options = opts;
 			this.has_tabs = false;
 			this.panels = _([
-				new AppearancePanel({model: this.model})
+				new PresetsPanel({model: this.model})
 			]);
 		},
 
