c6e1fa29ec371fff398e49ce69af51a6cbbe83c8
vladislav <ve@incsub.com>
2014-11-06 07:33:22 +0100

    Make use of the prepared strings in templates.

--- elements/upfront-posts/tpl/edition_bar.php
+++ elements/upfront-posts/tpl/edition_bar.php
@@ -1,11 +1,11 @@
 <div class="ueditor-bar ueditable" id="{{cid}}">
 	<div class="ueditor-bar-buttons">
-		<a href="#" class="ueditor-bar-show_advanced ueditor-bar-icon ueditor-bar-icon-only" title="Advanced tools"></a>
+		<a href="#" class="ueditor-bar-show_advanced ueditor-bar-icon ueditor-bar-icon-only" title="{{Upfront.Settings.l10n.global.content.advanced_tools}}"></a>
 		{[ if(cancelButton){ ]}
-		<a class="ueditor-action-cancel ueditor-bar-icon" href="#">Cancel</a>
+		<a class="ueditor-action-cancel ueditor-bar-icon" href="#">{{Upfront.Settings.l10n.global.content.cancel}}</a>
 		{[ } ]}
 		{[ if(draftButton){ ]}
-		<a class="ueditor-action-draft" href="#">Save draft</a>
+		<a class="ueditor-action-draft" href="#">{{Upfront.Settings.l10n.global.content.save_draft}}</a>
 		{[ } ]}
 		<a class="ueditor-action-publish ueditor-bar-icon" href="#">{{buttonText}}</a>
 	</div>
@@ -21,9 +21,9 @@
 			<div class="ueditor-select-wrapper ueditor-select-visibility">
 				<a class="ueditor-select-value ueditor-bar-icon ueditor-bar-icon-selectable" data-id="visibility">{{visibility.name}}</a>
 				<div class="ueditor-pass-editor">
-					<span>Password:</span>
+					<span>{{Upfront.Settings.l10n.global.content.password}}:</span>
 					<input type="text" class="upfront-field-text ueditor-pass" value="{{post_password}}">
-					<a class="button ueditor-pass-ok small-button">Ok</a>
+					<a class="button ueditor-pass-ok small-button">{{Upfront.Settings.l10n.global.content.ok}}</a>
 				</div>
 			</div>
 		</div>
@@ -32,9 +32,9 @@
 			<a class="ueditor-action-schedule ueditor-bar-icon ueditor-bar-icon-selectable">{{schedule.text}}</a>
 			{{datepicker}}
 		</div>	
-		<div class="ueditor-row-item"><a href="#" class="ueditor-action-tags ueditor-bar-icon ueditor-bar-icon-only" title="Edit Categories and Tags"></a></div>
-		<div class="ueditor-row-item"><a href="#" class="ueditor-action-url ueditor-bar-icon ueditor-bar-icon-only" title="Edit URL"></a></div>		
-		<div class="ueditor-row-item"><a href="#" class="ueditor-action-trash ueditor-bar-icon ueditor-bar-icon-only" title="Trash"></a></div>
+		<div class="ueditor-row-item"><a href="#" class="ueditor-action-tags ueditor-bar-icon ueditor-bar-icon-only" title="{{Upfront.Settings.l10n.global.content.edit_tax}}"></a></div>
+		<div class="ueditor-row-item"><a href="#" class="ueditor-action-url ueditor-bar-icon ueditor-bar-icon-only" title="{{Upfront.Settings.l10n.global.content.edit_url}}"></a></div>		
+		<div class="ueditor-row-item"><a href="#" class="ueditor-action-trash ueditor-bar-icon ueditor-bar-icon-only" title="{{Upfront.Settings.l10n.global.content.trash}}"></a></div>
 		
 	</div>
 </div>
