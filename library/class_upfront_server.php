13935236b9f90d27061b688109757c9195ee9bf7
vladislav <ve@incsub.com>
2014-11-09 08:18:03 +0100

    Fix double-gzipping the server responses when response debugging isn't active.

--- library/class_upfront_server.php
+++ library/class_upfront_server.php
@@ -29,7 +29,9 @@ abstract class Upfront_Server implements IUpfront_Server {
 	}
 
 	protected function _out (Upfront_HttpResponse $out) {
-		if (!$this->_debugger->is_active(Upfront_Debug::RESPONSE) && extension_loaded('zlib')) ob_start('ob_gzhandler');
+		if (!$this->_debugger->is_active(Upfront_Debug::RESPONSE) && extension_loaded('zlib') && 0 === ob_get_level()) {
+			ob_start('ob_gzhandler');
+		}
 		status_header($out->get_status());
 		header("Content-type: " . $out->get_content_type() . "; charset=utf-8");
 		die($out->get_output());
