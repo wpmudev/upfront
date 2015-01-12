d725c53b76be26f7908ee6b0f31781566341f185
vladislav <ve@incsub.com>
2015-01-12 08:32:49 +0100

    Refactor the postmeta model.

--- elements/upfront-posts/lib/class_upfront_posts_model.php
+++ elements/upfront-posts/lib/class_upfront_posts_model.php
@@ -9,67 +9,9 @@ class Upfront_Posts_Model {
 		return call_user_func(array($class_name, 'get_posts'), $data); //$class_name::get_posts($data);
 	}
 
-
-	/**
-	 * Returs a hash of meta fields (in key=>value format) for a post.
-	 *
-	 * @param int $post_id The ID of the post to search
-	 * @return array A hash of meta fields.
-	*/
-	public static function get_all_post_meta_fields ($post_id=false) {
-		if (empty($post_id) || !is_numeric($post_id)) return array();
-		global $wpdb;
-		$fields = $wpdb->get_results(
-			$wpdb->prepare("SELECT DISTINCT meta_key, meta_value FROM {$wpdb->postmeta} WHERE post_id=%d", $post_id),
-			ARRAY_A
-		);
-		return $fields;
-	}
-
-	/**
-	 * Returns a hash of requested key/value pairs for a post.
-	 *
-	 * @param int $post_id The ID of the post to search
-	 * @param array $fields A list of fields to search for
-	 * @return array
-	*/
-	public static function get_post_meta_fields ($post_id=false, $fields=array()) {
-		if (empty($post_id) || !is_numeric($post_id)) return array();
-		if (empty($fields) || !is_array($fields)) return array();
-
-		$safe_fields = array();
-		foreach ($fields as $field) {
-			$field = trim(preg_replace('/[^-_a-z.0-9]/i', '', $field));
-			if (!in_array($field, $safe_fields)) $safe_fields[] = $field;
-		}
-		if (empty($safe_fields)) return array();
-		$in = join("','", $safe_fields);
-
-		global $wpdb;
-		$fields = $wpdb->get_results(
-			$wpdb->prepare("SELECT DISTINCT meta_key, meta_value FROM {$wpdb->postmeta} WHERE post_id=%d AND meta_key IN('{$in}')", $post_id),
-			ARRAY_A
-		);
-		return $fields;
-	}
-
 	public static function get_meta_fields ($data) {
 		$posts = self::get_posts($data);
-		if (empty($posts)) return array();
-
-		$post_ids = array();
-		foreach ($posts as $post) {
-			if (empty($post->ID)) continue;
-			$post_ids[] = $post->ID;
-		}
-		if (empty($post_ids)) return array();
-
-		global $wpdb;
-		$in = join("', '", $post_ids);
-		$fields = $wpdb->get_col(
-			"SELECT DISTINCT meta_key FROM {$wpdb->postmeta} WHERE post_id IN('{$in}')"
-		);
-		return $fields;
+		return Upfront_PostmetaModel::get_meta_fields($posts);
 	}
 
 	public static function spawn_query ($data) {
