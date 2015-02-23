e1321ebc04611501b98250f3861a566aeaf2ba4e
vladislav <ve@incsub.com>
2015-02-23 09:21:43 +0100

    Add simple core dependency queue implementation.

--- library/class_upfront_registry.php
+++ library/class_upfront_registry.php
@@ -19,7 +19,7 @@ abstract class Upfront_Registry implements IUpfront_Registry {
 	protected function __construct () {}
 
 	public function __get ($key) {
-		return $this->_get($key);
+		return $this->get($key);
 	}
 
 	public function __set ($key, $value) {
@@ -70,5 +70,54 @@ class Upfront_PublicScripts_Registry extends Upfront_Registry {
 	public static function get_instance () {
 		if (!self::$_instance) self::$_instance = new self;
 		return self::$_instance;
+	}
+}
+
+
+class Upfront_CoreDependencies_Registry extends Upfront_Registry {
+
+	private static $_instance;
+
+	const SCRIPTS = 'scripts';
+	const STYLES = 'styles';
+
+	public static function get_instance () {
+		if (!self::$_instance) self::$_instance = new self;
+		return self::$_instance;
+	}
+
+	public function set ($key, $value) {
+		if (!in_array($key, array(self::SCRIPTS, self::STYLES))) return false;
+		if (empty($this->_data[$key])) $this->_data[$key] = array();
+
+		$this->_data[$key][] = $value;
+	}
+
+	public function add_script ($url) {
+		return $this->set(self::SCRIPTS, $url);
+	}
+	public function add_style ($url) {
+		return $this->set(self::STYLES, $url);
+	}
+
+	public function get ($key, $default=false) {
+		if (!in_array($key, array(self::SCRIPTS, self::STYLES))) return $default;
+		if (!is_array($this->_data[$key])) return $default;
+		
+		return current($this->_data[$key]);
+	}
+
+	public function get_scripts () {
+		return empty($this->_data[self::SCRIPTS])
+			? array()
+			: $this->_data[self::SCRIPTS]
+		;
+	}
+
+	public function get_styles () {
+		return empty($this->_data[self::STYLES])
+			? array()
+			: $this->_data[self::STYLES]
+		;
 	}
 }
