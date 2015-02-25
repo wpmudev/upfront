81e25d889e9dfdf72d4fea48df244957bd7b311c
vladislav <ve@incsub.com>
2015-02-25 08:44:52 +0100

    Add fonts-specific core dependencies registry API.

--- library/class_upfront_registry.php
+++ library/class_upfront_registry.php
@@ -80,6 +80,7 @@ class Upfront_CoreDependencies_Registry extends Upfront_Registry {
 
 	const SCRIPTS = 'scripts';
 	const STYLES = 'styles';
+	const FONTS = 'fonts';
 
 	public static function get_instance () {
 		if (!self::$_instance) self::$_instance = new self;
@@ -87,17 +88,56 @@ class Upfront_CoreDependencies_Registry extends Upfront_Registry {
 	}
 
 	public function set ($key, $value) {
-		if (!in_array($key, array(self::SCRIPTS, self::STYLES))) return false;
+		if (!in_array($key, array(self::SCRIPTS, self::STYLES, self::FONTS))) return false;
 		if (empty($this->_data[$key])) $this->_data[$key] = array();
 
 		$this->_data[$key][] = $value;
 	}
 
+	private function _set_all ($key, $values) {
+		if (!in_array($key, array(self::SCRIPTS, self::STYLES, self::FONTS))) return false;
+		if (empty($this->_data[$key])) $this->_data[$key] = array();
+
+		$this->_data[$key] = $values;
+	}
+
+	/**
+	 * Set individual script for inclusion.
+	 *
+	 * @param string $url External URL to load the script from
+	 */
 	public function add_script ($url) {
 		return $this->set(self::SCRIPTS, $url);
 	}
+
+	/**
+	 * Set individual style for inclusion.
+	 *
+	 * @param string $url External URL to load the style from
+	 */
 	public function add_style ($url) {
 		return $this->set(self::STYLES, $url);
+	}
+
+	/**
+	 * Set individual font for inclusion.
+	 *
+	 * Since we want to store the fonts as hash, this is a bit more complex method
+	 * that keeps the simple list of variants as values of font family key.
+	 *
+	 * @param string $family Font family to use.
+	 * @param mixed $variant Either a single string variant (weight), or a list of required variants to load
+	 */
+	public function add_font ($family, $variant=false) {
+		$fonts = $this->get_fonts();
+		
+		if (!isset($fonts[$family])) $fonts[$family] = array();
+		if (!empty($variant)) {
+			if (!is_array($variant)) $fonts[$family][] = $variant;
+			else $fonts[$family] = array_merge($fonts[$family], $variant);
+		}
+		
+		return $this->_set_all(self::FONTS, $fonts);
 	}
 
 	public function get ($key, $default=false) {
@@ -107,6 +147,11 @@ class Upfront_CoreDependencies_Registry extends Upfront_Registry {
 		return current($this->_data[$key]);
 	}
 
+	/**
+	 * Get all scripts registered this far.
+	 *
+	 * @return array Ordered list of scripts to include.
+	 */
 	public function get_scripts () {
 		return empty($this->_data[self::SCRIPTS])
 			? array()
@@ -114,10 +159,27 @@ class Upfront_CoreDependencies_Registry extends Upfront_Registry {
 		;
 	}
 
+	/**
+	 * Get all styles registered this far.
+	 *
+	 * @return array Ordered list of styles to include.
+	 */
 	public function get_styles () {
 		return empty($this->_data[self::STYLES])
 			? array()
 			: $this->_data[self::STYLES]
+		;
+	}
+
+	/**
+	 * Get all fonts registered this far.
+	 *
+	 * @return array Fonts hash, where each key is a font family and its values are required variants.
+	 */
+	public function get_fonts () {
+		return empty($this->_data[self::FONTS])
+			? array()
+			: $this->_data[self::FONTS]
 		;
 	}
 }
