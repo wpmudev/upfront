f640af924a4dc707fb48ee2849c565262aa6e4e6
vladislav <ve@incsub.com>
2015-02-26 10:37:31 +0100

    Do not drop shadow region on building preview.

--- scripts/upfront/upfront-views-editor.js
+++ scripts/upfront/upfront-views-editor.js
@@ -1292,7 +1292,7 @@ define([
 			Upfront.Events.on("command:layout:save", this.on_save, this);
 			Upfront.Events.on("command:layout:save_as", this.on_save, this);
 			Upfront.Events.on("command:layout:publish", this.on_save, this);
-			Upfront.Events.on("command:layout:preview", this.on_preview, this);
+			//Upfront.Events.on("command:layout:preview", this.on_preview, this); // Do NOT drop shadow region from layout on preview build
 			Upfront.Events.on("command:layout:save_success", this.on_save_after, this);
 			Upfront.Events.on("command:layout:save_error", this.on_save_after, this);
 			Upfront.Events.on("entity:drag_stop", this.reset_modules, this);
