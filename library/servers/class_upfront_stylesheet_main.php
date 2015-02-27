7b7e49d73e56fc3984aa37c28be93a81acbd1d2b
vladislav <ve@incsub.com>
2015-02-27 08:31:00 +0100

    Expose theme color styles getter for filtering.

--- library/servers/class_upfront_stylesheet_main.php
+++ library/servers/class_upfront_stylesheet_main.php
@@ -275,6 +275,6 @@ class Upfront_StylesheetMain extends Upfront_Server {
     }
 
     private function _get_theme_colors_styles(){
-        return get_option("upfront_" . get_stylesheet() . "_theme_colors_styles");
+        return apply_filters('upfront-get_theme_colors_styles', get_option("upfront_" . get_stylesheet() . "_theme_colors_styles"));
     }
 }
