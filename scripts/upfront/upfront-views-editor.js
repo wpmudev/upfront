f20e9717a0800264265588e10f8c71bdbcab129f
Sam Najian <dev.samnajian@gmail.com>
2015-03-17 13:53:15 +0330

    hide color picker when clicked outside of it

--- scripts/upfront/upfront-views-editor.js
+++ scripts/upfront/upfront-views-editor.js
@@ -4115,7 +4115,8 @@ var Field_ToggleableText = Field_Text.extend({
 
 				me.$(".sp-container").append("<div class='color_picker_rgb_container'></div>");
 				me.update_input_border_color(me.get_saved_value());
-
+				me.$(".sp-container").data("field_color", me);
+				me.$(".sp-container").data("$spectrum", me.$spectrum );
 				me.$(".sp-container").find(".sp-choose").on("click.spectrum", function(e){
 					if(me.options.spectrum && me.options.spectrum.choose && me.color)
 						me.options.spectrum.choose(me.color);
@@ -4125,7 +4126,6 @@ var Field_ToggleableText = Field_Text.extend({
 						me.$(".sp-container").addClass("sp-hidden");
 					}
 				});
-
 
 			});
 
