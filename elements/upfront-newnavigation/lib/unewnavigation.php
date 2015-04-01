afe772465d63c5c30096d6297468bb7af4dfd609
Gagan Goraya <gagan@incsub.com>
2015-04-01 18:10:56 +0530

    taking out the previous fix for the sake of optimization

--- elements/upfront-newnavigation/lib/unewnavigation.php
+++ elements/upfront-newnavigation/lib/unewnavigation.php
@@ -97,15 +97,14 @@ class Upfront_UnewnavigationView extends Upfront_Object {
 	}
 
 	public static  function add_styles_scripts() {
-		//upfront_add_element_style('upfront_navigation', array('css/unewnavigation-style.css', dirname(__FILE__)));
-		wp_enqueue_style('upfront_navigation', upfront_element_url('css/unewnavigation-style.css', dirname(__FILE__)));
-		
+		upfront_add_element_style('upfront_navigation', array('css/unewnavigation-style.css', dirname(__FILE__)));
+
 		if (is_user_logged_in()) {
 			upfront_add_element_style('upfront_navigation_editor', array('css/unewnavigation-editor.css', dirname(__FILE__)));
 		}
-		
-		
 		/*
+		wp_enqueue_style('upfront_navigation', upfront_element_url('css/unewnavigation-style.css', dirname(__FILE__)));
+
 		if (is_user_logged_in()) {
 			wp_enqueue_style('unewnavigation_editor', upfront_element_url('css/unewnavigation-editor.css', dirname(__FILE__)));
 		}
