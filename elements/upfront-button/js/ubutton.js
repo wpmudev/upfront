95aa483a88fe8a40fb8e43968001dd9f6190624f
Cvetan Cvetanov <cvetan@incsub.com>
2015-05-12 14:32:33 +0300

    Show only one link panel instance

--- elements/upfront-button/js/ubutton.js
+++ elements/upfront-button/js/ubutton.js
@@ -135,6 +135,8 @@ var ButtonView = Upfront.Views.ObjectView.extend({
 	onOpenItemControlsClick: function() {
 		this.$el.toggleClass('controls-visible');
 		if (this.$el.hasClass('controls-visible')) {
+			$('.upfront-button.controls-visible').removeClass('controls-visible');
+			this.$el.addClass('controls-visible');
 			this.controlsVisible = true;
 		} else {
 			this.controlsVisible = false;
