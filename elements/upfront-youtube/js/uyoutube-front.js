e7da5e4724a46601fdeefa21b904518dea110016
Cvetan Cvetanov <cvetan@incsub.com>
2015-05-14 14:13:50 +0300

    Fix selecting wrong iframe

--- elements/upfront-youtube/js/uyoutube-front.js
+++ elements/upfront-youtube/js/uyoutube-front.js
@@ -3,7 +3,7 @@
 		$('.uyoutube-gallery-item, .uyoutube-list-item').on('click', function(event) {
       
 			if($(this).hasClass('firstHidden')) {
-				var existing_videoUrl = $('.uyoutube-main-video iframe').attr('src');
+				var existing_videoUrl = $(this).parent().prev().find('iframe').attr('src');
 				
 				if(existing_videoUrl) {
 					videoId = existing_videoUrl.match(/^(https?:\/\/(www\.)?)?youtube\.com\/embed\/([-_A-Za-z0-9]+)/)[3];
