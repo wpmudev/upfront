592d3a80d117d175662eb82247e491ae3e251ce7
Cvetan Cvetanov <cvetan@incsub.com>
2015-05-19 10:35:24 +0300

    Add Chose plugin for Preset Select

--- scripts/upfront/preset-settings/select-preset-field.js
+++ scripts/upfront/preset-settings/select-preset-field.js
@@ -1,12 +1,16 @@
 define(function() {
-	var SelectPresetField = Upfront.Views.Editor.Field.Select.extend({
+	var SelectPresetField = Upfront.Views.Editor.Field.Chosen_Select.extend({
 		className: 'preset select-preset-field',
 
 		render: function() {
-			Upfront.Views.Editor.Field.Select.prototype.render.call(this);
+			Upfront.Views.Editor.Field.Chosen_Select.prototype.render.call(this);
 			var me = this;
 			var html = ['<a href="#" title="Edit preset" class="upfront-preset-edit">edit preset</a>'];
 			this.$el.append(html.join(''));
+			
+			this.$el.find('.upfront-chosen-select').chosen({
+				width: '230px'
+			});
 
 			this.$el.on('click', '.upfront-preset-edit', function(event) {
 				event.preventDefault();
@@ -14,11 +18,11 @@ define(function() {
 				if (me.get_value() === 'default') {
 					alert('Default preset can not be edited.');
 					return;
-				}
+				} 
 				me.trigger('upfront:presets:edit', me.get_value());
 			});
 			return this;
-		}
+		},
 	});
 
 	return SelectPresetField;
