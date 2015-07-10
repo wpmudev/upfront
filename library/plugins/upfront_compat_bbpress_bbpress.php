003677e46d7ff764673409513215cafc422589f2
Gagan Goraya <gagan@incsub.com>
2015-07-10 20:12:18 +0530

    second part of the merge glitch

--- library/plugins/upfront_compat_bbpress_bbpress.php
+++ library/plugins/upfront_compat_bbpress_bbpress.php
@@ -11,12 +11,14 @@ class Upfront_Compat_Bbpress_Bbpress implements IUpfront_Server {
     private function _add_hooks () {
         add_theme_support( 'bbpress' );
         add_action('wp', array($this, 'detect_virtual_page'));
+        add_action('wp_ajax_upfront_posts-load', array($this, "load_posts"), 9);
     }
 
     public function detect_virtual_page () {
 
         if(is_bbpress()) { // if it is a bbPress page
             add_filter('upfront-views-view_class', array($this, 'override_view'));
+            add_filter('upfront-entity_resolver-entity_ids', array($this, 'resolve_entity_ids'));
         }
             
     }
@@ -26,7 +28,37 @@ class Upfront_Compat_Bbpress_Bbpress implements IUpfront_Server {
         return $view_class;
     }
 
-    
+    public function resolve_entity_ids ($cascade) {
+        //$cascade['item'] = "wtf wtf";
+
+        return $cascade;
+
+    }
+
+    public function load_posts () {
+        
+        $data = stripslashes_deep($_POST);
+
+
+        ob_start();
+        echo "new one dad";
+        var_dump($data['layout']);
+        file_put_contents("debugg.txt", ob_get_clean()); 
+
+       // if (empty($data['layout']['item']) && empty($data['layout']['specificity'])) return false; // Don't deal with this if we don't know what it is
+
+       /* $has_woo_item = !empty($data['layout']['item']) && (bool)preg_match('/^woocommerce/', $data['layout']['item']);
+        $has_woo_spec = !empty($data['layout']['specificity']) && (bool)preg_match('/^woocommerce/', $data['layout']['specificity']);
+
+        if (!$has_woo_item && !$has_woo_spec) return false;
+*/
+       /* echo " hi wassup";
+        $this->_out(new Upfront_JsonResponse_Success(array(
+            'posts' => '<div class="upfront-woocommerce_compat upfront-plugin_compat"><p>WooCommerce specific content</p></div>',
+            'pagination' => '',
+        )));
+        */
+    }
 
 }
 
