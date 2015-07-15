dbf415e765b2855f99359d67bb845fa634e4c34f
Jeffri Tjin <jeffri@incsub.com>
2015-07-15 15:21:52 +0700

    Vertical resize to snap into baseline grid, fix resize handle appearance.

--- scripts/upfront/upfront-behaviors.js
+++ scripts/upfront/upfront-behaviors.js
@@ -2824,7 +2824,7 @@ var GridEditor = {
 					width: ((me.col/(me.col+margin.original.left))*100) + '%',
 					height: ui.originalSize.height
 				});
-				$resize = $('<div class="upfront-resize" style="height:'+me.height+'px;"></div>');
+				/*$resize = $('<div class="upfront-resize" style="height:'+me.height+'px;"></div>');
 				$resize.css({
 					height: me.height,
 					width: me.width,
@@ -2844,7 +2844,7 @@ var GridEditor = {
 						top: me.position.top,
 						left: me.position.left
 					});
-				$('body').append($resize);
+				$('body').append($resize);*/
 				// Refreshing the elements position
 				_.each(ed.els, function(each, index){
 					ed.els[index] = ed.get_position(each.$el);
@@ -2905,19 +2905,19 @@ var GridEditor = {
 					$(window).scrollTop( $(window).scrollTop()+(ed.baseline*10) );
 				}
 				$me.css({
-					height: h,
+					height: rsz_row*ed.baseline,
 					width: w,
 					minWidth: w,
 					maxWidth: w
 				});
 				$me.data('resize-col', rsz_col);
 				$me.data('resize-row', rsz_row);
-				$resize.css({
+				/*$resize.css({
 					height: rsz_row*ed.baseline,
 					width: rsz_col*ed.col_size,
 					minWidth: rsz_col*ed.col_size,
 					maxWidth: rsz_col*ed.col_size,
-				});
+				});*/
 				if(axis == 'nw') {
 					$resize.css({
 						top: me.$el.find('>.upfront-resize-handle-nw').offset().top,
@@ -2953,7 +2953,7 @@ var GridEditor = {
 				ed.resizing = false;
 
 				$resize_placeholder.remove();
-				$resize.remove();
+				//$resize.remove();
 
 				ed.update_class($me, ed.grid.class, rsz_col);
 				if ( axis == 'nw' ){
@@ -3189,8 +3189,8 @@ var GridEditor = {
 			$me.resizable('option', 'disabled', false);
 			return false;
 		}
-		$me.append('<span class="upfront-resize-handle-wrapper upfront-resize-handle-w ui-resizable-handle ui-resizable-w">');
-		$me.append('<span class="upfront-resize-handle-wrapper upfront-resize-handle-e ui-resizable-handle ui-resizable-e">');
+		//$me.append('<span class="upfront-resize-handle-wrapper upfront-resize-handle-w ui-resizable-handle ui-resizable-w">');
+		//$me.append('<span class="upfront-resize-handle-wrapper upfront-resize-handle-e ui-resizable-handle ui-resizable-e">');
 		$me.resizable({
 			containment: "document",
 			autoHide: true,
