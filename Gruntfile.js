d23adceee6004459d0bd877597f2017a09d295ee
vladislav <ve@incsub.com>
2015-07-20 08:57:37 +0200

    Minify `global.css` stylesheet.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,8 @@
 module.exports = function(grunt) {
 	require('load-grunt-tasks')(grunt);
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks( 'grunt-wp-i18n' );
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-wp-i18n');
 
 	grunt.initConfig({
 		sass: {
@@ -15,6 +16,12 @@ module.exports = function(grunt) {
 				files: {
 					'styles/editor-interface.css': 'styles/editor-interface.scss'
 				}
+			}
+		},
+		cssmin: {
+			minify: {
+				src: 'styles/global.css',
+				dest: 'styles/global.min.css'
 			}
 		},
 		watch: {
