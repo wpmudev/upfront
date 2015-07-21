3d0d3023a5a282907c8a30a6698b4995fc546c71
Cvetan <cvetan@incsub.com>
2015-07-21 18:36:23 +0300

    Add select box module

---
+++ scripts/upfront/preset-settings/selectbox-settings-item.js
@@ -0,0 +1,34 @@
+/*
+* Field names properies
+* `name` - Select box name
+*/
+define(function() {	
+	var l10n = Upfront.Settings.l10n.preset_manager;
+	var SelectboxSettingsItem = Upfront.Views.Editor.Settings.Item.extend({
+		className: 'settings_module selectbox_settings_item clearfix',
+		group: false,
+		
+		initialize: function(options) {
+			this.options = options || {};
+			var me = this,
+				custom_class = this.options.className,
+				state = this.options.state;
+
+			this.fields = _([
+				new Upfront.Views.Editor.Field.Select({
+					model: this.model,
+					className: state + '-select select-module ' + custom_class,
+					name: me.options.fields.name,
+					default_value: me.options.default_value,
+					label: '',
+					values: me.options.values,
+					change: function(value) {
+						me.model.set(me.options.fields.name, value);
+					}
+				})
+			]);
+		},
+	});
+
+	return SelectboxSettingsItem;
+});
