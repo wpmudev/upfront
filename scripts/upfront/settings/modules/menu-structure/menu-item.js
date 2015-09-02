439c707b31bdce990732a0dd07d3a3ec41ed81e7
Ivan Ivanić <spoygg@spoygg.com>
2015-09-02 10:37:04 +0200

    Rendering sub-items to menu structure module

--- scripts/upfront/settings/modules/menu-structure/menu-item.js
+++ scripts/upfront/settings/modules/menu-structure/menu-item.js
@@ -3,7 +3,25 @@ define([
 	var MenuItem = Backbone.View.extend({
 		className: 'menu-structure-module-item',
 
+		initialize: function(options) {
+			this.options = options || {};
+			this.subViews = [];
+			var sub = this.model.get('sub');
+
+			if (sub) {
+				_.each(sub, function(itemOptions) {
+					this.subViews.push(
+						new MenuItem({
+							model: new Backbone.Model(itemOptions),
+							depth: this.depth + 1
+						})
+					);
+				}, this);
+			}
+		},
+
 		render: function() {
+			var me = this;
 			// menu-item-db-id: 586
 			// menu-item-object: "custom"
 			// menu-item-object-id: "586"
@@ -14,6 +32,11 @@ define([
 			// menu-item-type: "custom"
 			// menu-item-url: "http://local.wordpress.dev/"
 			this.$el.html(this.model.get('menu-item-title') + ' ' + this.model.get('menu-item-type'));
+			this.$el.data('menu-item-object-id', this.model.get('menu-item-object-id'));
+
+			_.each(me.subViews, function(view) {
+				me.$el.append(view.render().el);
+			});
 
 			return this;
 		}
