b64e136366a051ce02a50b2504fb430df2d9fe57
vladislav <ve@incsub.com>
2015-10-23 07:14:09 +0200

    Fixed missing cache key issue in dev mode.

--- library/servers/class_upfront_element_styles.php
+++ library/servers/class_upfront_element_styles.php
@@ -38,7 +38,7 @@ class Upfront_ElementStyles extends Upfront_Server {
 	 */
 	public function load_styles () {
 		$raw_cache_key = $this->_get_cached_styles();
-		wp_enqueue_style('upfront-element-styles', $this->_get_enqueueing_url(self::TYPE_STYLE, $raw_cache_key), array(), $this->_get_enqueue_version()); // But let's do pretty instead
+		if (!empty($raw_cache_key)) wp_enqueue_style('upfront-element-styles', $this->_get_enqueueing_url(self::TYPE_STYLE, $raw_cache_key), array(), $this->_get_enqueue_version()); // But let's do pretty instead
 	}
 
 	/**
@@ -146,7 +146,7 @@ class Upfront_ElementStyles extends Upfront_Server {
 	 */
 	public function load_scripts () {
 		$raw_cache_key = $this->_get_cached_scripts();
-		wp_enqueue_script('upfront-element-scripts', $this->_get_enqueueing_url(self::TYPE_SCRIPT, $raw_cache_key), array('jquery'), $this->_get_enqueue_version(), true); // Scripts go into footer
+		if (!empty($raw_cache_key)) wp_enqueue_script('upfront-element-scripts', $this->_get_enqueueing_url(self::TYPE_SCRIPT, $raw_cache_key), array('jquery'), $this->_get_enqueue_version(), true); // Scripts go into footer
 	}
 
 	/**
