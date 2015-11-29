301cef470b0f1355ae0d4ebf0126ce1536d6677e
Gagan Goraya <gagan@incsub.com>
2015-11-29 13:51:49 +0530

    contact form element: modification to the email headers
    https://www.meistertask.com/app/task/vv4AfK8w/contact-element-no-email-is-received

--- elements/upfront-contact-form/lib/contact_form.php
+++ elements/upfront-contact-form/lib/contact_form.php
@@ -238,13 +238,13 @@ class Upfront_UcontactView extends Upfront_Object {
 			if (empty($emailto)) $emailto = get_option('admin_email');
 			if (!is_email($emailto)) $emailto = false;
 
-			$headers = array('From: ' . "{$name} <{$email}>", 'Reply-To: ' . $email);
-
+			$headers[] = 'From: ' . $name . ' <' . $email . ">\r\n";
 			$this->msg = $this->check_fields($name, $email, $subject, $message);
-
+			
 			if ($this->msg) {
 				$this->msg_class = 'error';
 			} else if (!empty($emailto)) {
+				
 				// Let's first force mail callbacks
 				if (!empty($name)) {
 					$email_callback = create_function('$email', "return '{$email}';");
@@ -255,6 +255,7 @@ class Upfront_UcontactView extends Upfront_Object {
 
 				// ... then send email
 				if (!wp_mail($emailto, $subject, $message, $headers)) {
+					
 					$this->msg = self::_get_l10n('error_sending');
 					$this->msg_class = 'error';
 				} else $this->msg = self::_get_l10n('mail_sent');
