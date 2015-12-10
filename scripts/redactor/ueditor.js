a17d4439347641f60299a1b6107ac19da0f893e8
Gagan Goraya <gagan@incsub.com>
2015-12-10 15:31:45 +0530

    fixed Text Link Target Tooltips, the Link To Anchor tooltip & text is wrong.
    https://www.meistertask.com/app/task/ysqCDyFB/master-text-link-target-tooltips-the-link-to-anchor-tooltip-text-is-wrong

--- scripts/redactor/ueditor.js
+++ scripts/redactor/ueditor.js
@@ -904,7 +904,7 @@ Ueditor.prototype = {
 			if($(this).find('i.visit_link').length > 0 || !$(this).attr('href') || $(this).text().trim() == '')
 				return;
 			$(this).css('position', 'relative');
-			$(this).append('<i class="visit_link visit_link_'+me.guessLinkType($(this).attr('href'))+'" data-href="'+$(this).attr('href')+'"></i>');
+			$(this).append('<i class="visit_link visit_link_'+me.guessLinkTypeTag($(this))+'" data-href="'+$(this).attr('href')+'"></i>');
 			$(this).removeAttr('href');
 			//$(this).attr('onclick', 'return false;');
 		});
@@ -942,12 +942,30 @@ Ueditor.prototype = {
 		else
 			window.open(url);
 	},
+    /*validateEmail: function (email) {
+        var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    },*/
+    guessLinkTypeTag: function(item){
+        return item.data('upfront-link-type');
+    },
 	guessLinkType: function(url){
-
+        var anchor = false;
 		if(!$.trim(url) || $.trim(url) == '#')
 			return 'unlink';
-		if(url.length && url[0] == '#')
-			return url.indexOf('#ltb-') > -1 ? 'lightbox' : 'anchor';
+
+        if(url.indexOf('#') > -1) {
+            anchor =this.getUrlanchor(url);
+        }
+
+		if(anchor) {
+			return url.indexOf('ltb-') > -1 ? 'lightbox' : 'anchor';
+        }
+        // is it an email.
+        if(url.indexOf('mailto:') === 0) {
+            return 'email';
+        }
+
 		if(url.substring(0, location.origin.length) == location.origin)
 			return 'entry';
 
