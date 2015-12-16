e754620f9f8ea797c0ca48c3594e2f6a1d107329
Cvetan <cvetan@incsub.com>
2015-12-16 10:55:24 +0200

    Fix existing preset migration css specificity

--- scripts/upfront/preset-settings/preset-manager.js
+++ scripts/upfront/preset-settings/preset-manager.js
@@ -249,10 +249,23 @@ define([
 				Upfront.mainData[this.mainDataCollection].splice(index, 1);
 			}
 			Upfront.mainData[this.mainDataCollection].push(properties);
+			
+			//Update styles to fix css specificity
+			properties = this.updateMigratedPresetStyles(preset, style);
+			
 			Util.updatePresetStyle(this.styleElementPrefix.replace(/-preset/, ''), properties, this.styleTpl);
 			// Trigger change so that whole element re-renders again.
 			// (to replace element style class with preset class, look upfront-views.js
 			this.model.get('properties').trigger('change');
+		},
+		
+		updateMigratedPresetStyles: function(preset, style) {
+			//Fix preset_style specificity on migration
+			preset.set({
+				preset_style: style.replace(/#page/g, '#page.upfront-layout-view .upfront-editable_entity.upfront-module')
+			});
+			
+			return preset.toJSON();
 		},
 
 		setupItems: function() {
