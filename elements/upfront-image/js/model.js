b9e2049f08d12e22d79670e92bdef3f937e322b3
Sam Najian <dev.samnajian@gmail.com>
2015-12-30 23:38:58 +0330

    uimage: set default caption to use p tag so that block level formatting works for it out of the box

--- elements/upfront-image/js/model.js
+++ elements/upfront-image/js/model.js
@@ -3,6 +3,11 @@ define(function() {
 		init: function () {
 			var properties = _.clone(Upfront.data.uimage.defaults);
 			properties.element_id = Upfront.Util.get_unique_id(properties.id_slug);
+			/**
+			 * Init with caption in p block so that block level formatting will work for the caption without any issues
+			 * @type {string}
+             */
+			properties.image_caption = "<p>$s</p>".replace("$s", properties.image_caption);
 			this.init_properties(properties);
 		}
 	});
