1e823f96b3f1fe1097da3632c24e0200037db28e
Cvetan <cvetan@incsub.com>
2016-01-08 10:34:41 +0200

    Fix slider region broken images

--- scripts/upfront/bg-settings/slider-item.js
+++ scripts/upfront/bg-settings/slider-item.js
@@ -158,16 +158,14 @@ define([
 					image_id = $image.data('image-id'),
 					slide_images = me.model.get_breakpoint_property_value('background_slider_images', true);
 
-				slide_images = _.map(slide_images, function(id){
-					return parseInt( id, 10 );
-				});
-
 				if (_.isString(image_id) && image_id.match(/^[0-9]+$/))
 					image_id = parseInt(image_id, 10);
 
 				slide_images = _.without(slide_images, image_id);
 				me.model.set_breakpoint_property('background_slider_images', slide_images);
 				$image.remove();
+				
+				me.update_slider_slides();
 			});
 			
 			this.on('show', function(){
