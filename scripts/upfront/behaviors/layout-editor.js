199360182044c2f56c66476bb67edd6e6e608fc6
vladislav <ve@incsub.com>
2016-02-22 09:10:38 +0100

    Actively check if we're in builder mode _before_ sending out builder-specific requests.
    @TODO: Builder-specific JS should be removed from core entirely.
    
    Fixes: https://www.meistertask.com/app/task/eSUnkV1R/scribe-console-error-when-saving-the-settings-of-elements
    Fixes: https://www.meistertask.com/app/task/qOu0GX9X/console-error-when-editing-default-preset-of-text-element

--- scripts/upfront/behaviors/layout-editor.js
+++ scripts/upfront/behaviors/layout-editor.js
@@ -1185,49 +1185,66 @@ var LayoutEditor = {
 	 */
 	_get_saved_layout: function (){
 		var me = this,
-			deferred = new $.Deferred();
-		Upfront.Util.post({
-			action: 'upfront_list_theme_layouts'
-		}).success(function(response){
-			me.saved_layouts = response.data;
-			deferred.resolve(response.data);
-		}).error(function(){
-			deferred.reject();
-		});
+			deferred = new $.Deferred()
+		;
+		
+		// The request should only ever be sent in builder mode
+		if (Upfront.Application.is_builder()) {
+			Upfront.Util.post({
+				action: 'upfront_list_theme_layouts'
+			}).success(function(response){
+				me.saved_layouts = response.data;
+				deferred.resolve(response.data);
+			}).error(function(){
+				deferred.reject();
+			});
+		} else setTimeout(deferred.reject);
+
 		return deferred.promise();
 	},
 
 	_get_themes: function () {
 		var me = this,
-			deferred = new $.Deferred();
-		Upfront.Util.post({
-			action: 'upfront_thx-get-themes'
-		}).success(function(response){
-			me.themes = response;
-			deferred.resolve(response);
-		}).error(function(){
-			deferred.reject();
-		});
+			deferred = new $.Deferred()
+		;
+		// The request should only ever be sent in builder mode
+		if (Upfront.Application.is_builder()) {
+			Upfront.Util.post({
+				action: 'upfront_thx-get-themes'
+			}).success(function(response){
+				me.themes = response;
+				deferred.resolve(response);
+			}).error(function(){
+				deferred.reject();
+			});
+		} else setTimeout(deferred.reject);
 		return deferred.promise();
 	},
 
 	_create_theme: function (data) {
 		var deferred = new $.Deferred();
-		Upfront.Util.post({
-			action: 'upfront_thx-create-theme',
-			form: this._build_query(data)
-		}).success(function(response){
-			if ( response && response.error )
-				deferred.reject(response.error);
-			else
-				deferred.resolve();
-		}).error(function(){
-			deferred.reject();
-		});
+
+		// The request should only ever be sent in builder mode
+		if (Upfront.Application.is_builder()) {
+			Upfront.Util.post({
+				action: 'upfront_thx-create-theme',
+				form: this._build_query(data)
+			}).success(function(response){
+				if ( response && response.error )
+					deferred.reject(response.error);
+				else
+					deferred.resolve();
+			}).error(function(){
+				deferred.reject();
+			});
+		} else setTimeout(deferred.reject);
 		return deferred.promise();
 	},
 
 	export_element_styles: function(data) {
+		// The request should only ever be sent in builder mode
+		if (!Upfront.Application.is_builder()) return false;
+
 		Upfront.Util.post({
 			action: 'upfront_thx-export-element-styles',
 			data: data
@@ -1251,8 +1268,15 @@ var LayoutEditor = {
 		var typography,
 			properties,
 			layout_style,
-			deferred,
-			data = {};
+			deferred new $.Deferred(),
+			data = {}
+		;
+
+		// The request should only ever be sent in builder mode
+		if (!Upfront.Application.is_builder()) {
+			setTimeout(deferred.reject);
+			return deferred.promise();
+		}
 
 		typography = _.findWhere(
 			Upfront.Application.current_subapplication.get_layout_data().properties,
@@ -1295,7 +1319,6 @@ var LayoutEditor = {
 
 		if (custom_data) data = _.extend(data, custom_data);
 
-		deferred = new $.Deferred();
 		Upfront.Util.post({
 			action: 'upfront_thx-export-layout',
 			data: data
