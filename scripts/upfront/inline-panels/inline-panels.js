905b4088d5c18ce3d5747ebd9b82818c8dc38347
Cvetan <cvetan@incsub.com>
2016-03-03 07:57:43 +0200

    Add new control for sub panels

--- scripts/upfront/inline-panels/inline-panels.js
+++ scripts/upfront/inline-panels/inline-panels.js
@@ -6,6 +6,7 @@ define([
 	'scripts/upfront/inline-panels/control',
 	'scripts/upfront/inline-panels/multi-control',
 	'scripts/upfront/inline-panels/tooltip-control',
+	'scripts/upfront/inline-panels/sub-control',
 	'scripts/upfront/inline-panels/padding-control',
 	'scripts/upfront/inline-panels/control-panel',
 	'scripts/upfront/inline-panels/dialog-control',
@@ -14,7 +15,7 @@ define([
 	'scripts/upfront/inline-panels/controls/visit-link',
 	'scripts/upfront/inline-panels/controls/link-panel',
 	'scripts/upfront/inline-panels/controls/group-link-panel'
-], function (Panels, Panel, Item, ItemMulti, Control, MultiControl, TooltipControl, PaddingControl,
+], function (Panels, Panel, Item, ItemMulti, Control, MultiControl, TooltipControl, SubControl, PaddingControl,
 	ControlPanel, DialogControl, ImageEditControl, CollapsedMultiControl, VisitLinkControl, LinkPanelControl, GroupLinkPanelControl) {
 
 	return {
@@ -25,6 +26,7 @@ define([
 		Control: Control,
 		MultiControl: MultiControl,
 		TooltipControl: TooltipControl,
+		SubControl: SubControl,
 		PaddingControl: PaddingControl,
 		ControlPanel: ControlPanel,
 		DialogControl: DialogControl,
---
+++ scripts/upfront/inline-panels/sub-control.js
@@ -0,0 +1,126 @@
+(function ($) {
+define([
+	'scripts/upfront/inline-panels/item',
+	'scripts/upfront/inline-panels/control'
+], function (Item, Control) {
+	var l10n = Upfront.mainData.l10n.image_element;
+
+	var SubControl = Control.extend({
+		multiControl: true,
+
+		events: {
+			'click': 'onClickControl',
+			'click .upfront-inline-panel-item': 'selectItem'
+		},
+
+		initialize: function() {
+			var me = this;
+			$(document).click(function(e){
+				var	target = $(e.target);
+
+				if(target.closest('#page').length && target[0] !== me.el && !target.closest(me.el).length && me.isOpen) {
+					me.close();
+				}
+			});
+		},
+
+		onClickControl: function(e){
+			if (this.isDisabled) {
+				return;
+			}
+
+			e.preventDefault();
+
+			this.clicked(e);
+
+			this.$el.siblings('.upfront-control-dialog-open').removeClass('upfront-control-dialog-open');
+
+			if (this.isOpen) {
+				this.close();
+			} else {
+				this.open();
+			}
+		},
+
+		open: function() {
+			this.isOpen = true;
+			this.$el.addClass('upfront-control-dialog-open');
+		},
+
+		close: function() {
+			this.isOpen = false;
+			this.$el.removeClass('upfront-control-dialog-open');
+		},
+
+		render: function() {
+			Item.prototype.render.call(this, arguments);
+			var captionControl = this.$('.image-sub-control'),
+				me = this,
+				selectedItem
+			;
+
+			if(!captionControl.length){
+				captionControl = $('<div class="image-sub-control inline-panel-control-dialog"></div>');
+				this.$el.append(captionControl);
+			}
+			_.each(this.sub_items, function(item, key){
+				if(key === me.selected){
+					item.setIsSelected(true);
+				} else {
+					item.setIsSelected(false);
+				}
+
+				item.render();
+				captionControl.append(item.$el);
+				me.listenTo(item, 'click', me.selectItem);
+			});
+
+			selectedItem = this.sub_items[this.selected];
+					if(selectedItem){
+							if( typeof selectedItem.icon !== 'undefined' ){
+									this.$el.children('i').addClass('upfront-icon-region-' + selectedItem.icon);
+							}else if( typeof selectedItem.label !== 'undefined' ){
+									this.$el.find('.tooltip-content').append( ': ' +  selectedItem.label );
+							}
+					}
+		},
+
+		get_selected_item: function () {
+			return this.selected;
+		},
+
+		selectItem: function(e){
+			var found = false,
+				target = $(e.target).is('i') ? $(e.target) : $(e.target).find('i');
+
+			_.each(this.sub_items, function(item, key){
+				if(target.hasClass('upfront-icon-region-' + item.icon)) {
+					found = key;
+				}
+
+				if( !found && $(e.target).closest('.upfront-inline-panel-item').attr('id') === item.id ){
+					found = key;
+				}
+
+			});
+
+			if(found){
+				this.selected = found;
+				this.render();
+				this.trigger('select', found);
+			}
+		},
+
+		setDisabled: function(isDisabled) {
+			this.isDisabled = isDisabled;
+			if (isDisabled) {
+				this.tooltip = l10n.ctrl.caption_position_disabled;
+			} else {
+				this.tooltip = l10n.ctrl.caption_display;
+			}
+		}
+	});
+
+	return SubControl;
+});
+})(jQuery);
