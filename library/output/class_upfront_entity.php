a6fb8614c7716806bdf71f5f106e9f2a2d2c9dca
Jeffri Tjin <jeffri@incsub.com>
2016-04-13 16:16:22 +0700

    Fix warning if featured image is defined but missing

--- library/output/class_upfront_entity.php
+++ library/output/class_upfront_entity.php
@@ -195,8 +195,10 @@ abstract class Upfront_Entity {
 			$background_default = $this->_get_breakpoint_property('background_default', $breakpoint_id);
 			if ($type == 'featured' && has_post_thumbnail(Upfront_Output::get_post_id())) {
 				$featured_image = wp_get_attachment_image_src( get_post_thumbnail_id( Upfront_Output::get_post_id() ), 'single-post-thumbnail' );
-				$background_image = $featured_image[0];
-				$background_image_ratio = round($featured_image[2]/$featured_image[1], 2);
+				if (!empty($featured_image)) {
+					$background_image = $featured_image[0];
+					$background_image_ratio = round($featured_image[2]/$featured_image[1], 2);
+				}
 			} else if (!$type || $type == 'image' || ($type == 'featured' && $background_default == 'image')) {
 				$background_image = $this->_get_breakpoint_property('background_image', $breakpoint_id);
 				$background_image_ratio = $this->_get_breakpoint_property('background_image_ratio', $breakpoint_id);
