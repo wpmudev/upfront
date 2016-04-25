13554f074a2adb11980c11df3294ab2df81f0405
vladislav <ve@incsub.com>
2016-04-25 09:07:21 +0200

    Refactor post data elements l10n data loading and fully translate new fields.

--- elements/upfront-post-data/js/modules-meta.js
+++ elements/upfront-post-data/js/modules-meta.js
@@ -2,12 +2,13 @@ define([
 	'elements/upfront-post-data/js/panel-abstractions',
 	'text!elements/upfront-post-data/tpl/preset-styles/meta.html'
 ], function (Panel, template) {
+	var l10n = Upfront.Settings.l10n.post_data_element;
 
 	var Modules = {};
 	Modules.template = template;
 
 	Modules.part_meta = Panel.Toggleable.extend({
-		title: "Meta",
+		title: l10n.meta.meta_part_title,
 		data_part: 'meta',
 		get_modules: function () { return []; } // No extra modules for meta
 	});
