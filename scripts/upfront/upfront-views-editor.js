1e091cb1470aedd7906988b113f8bdf8f8ee6814
vladislav <ve@incsub.com>
2016-06-18 08:36:12 +0200

    Client-side part of the quick fix.
    The "save_as" trigger doesn't seem to work for non-posts.
    Fixes issue Jeffri discovered with tags/category archives not being saved.

--- scripts/upfront/upfront-views-editor.js
+++ scripts/upfront/upfront-views-editor.js
@@ -672,7 +672,7 @@
 				}, 200);
 			},
 			on_click: function () {
-				if ( _upfront_post_data.layout.specificity && _upfront_post_data.layout.item && !_upfront_post_data.layout.item.match(/-page/) ) {
+				if (_upfront_post_data.post_id && _upfront_post_data.layout.specificity && _upfront_post_data.layout.item && !_upfront_post_data.layout.item.match(/-page/) ) {
 					Upfront.Events.trigger("command:layout:save_as");
 				} else {
 					Upfront.Events.trigger("command:layout:save");
@@ -2043,7 +2043,7 @@
 						}
 					});
 				}
-				
+
 				this.listenTo(Upfront.Views.PostDataEditor, 'loaded', function(contentEditor) {
 					if ( contentEditor ) {
 						Upfront.Views.PostBox = contentEditor.prepareBox();
@@ -4308,7 +4308,7 @@
 					})
 				);
 			},
-			
+
 			post_saved: function () {
 				// We should fetch colletion after post / page update to retrieve any title changes
 				this.collection.fetch();
