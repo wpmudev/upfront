148848f9800e03c9ffae9044de9c12c77e23a661
vladislav <ve@incsub.com>
2016-07-08 08:08:58 +0200

    Fix version l10n string.

--- library/admin/class_upfront_admin_general.php
+++ library/admin/class_upfront_admin_general.php
@@ -33,7 +33,8 @@ class Upfront_Admin_General extends Upfront_Admin_Page {
 								Upfront <span>V <?php echo esc_html($core_version); ?></span>
 							</div>
 							<div class="upfront-debug-block">
-								<?php echo $current->Name; ?> (Active Theme)<span>V <?php echo esc_html($child_version); ?></span>
+								<?php echo esc_html(sprintf(__('%s (Active Theme)', Upfront::TextDomain), $current->Name)); ?>
+									<span>V <?php echo esc_html($child_version); ?></span>
 							</div>
 							<?php if (class_exists('UpfrontThemeExporter') && is_callable(array('UpfrontThemeExporter', 'upfront_exporter_version'))) { ?>
 							<div class="upfront-debug-block">
