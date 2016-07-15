8d2c7fe63a7a0f471ddc1e0ec61966062bc89311
vladislav <ve@incsub.com>
2016-07-15 07:53:56 +0200

    Quick fix archive type fallback templates.

--- layouts/index.php
+++ layouts/index.php
@@ -8,7 +8,7 @@ $left_sidebar = !empty($left_sidebar) ? $left_sidebar : false;
 $right_sidebar = !empty($right_sidebar) ? $right_sidebar : false;
 
 $main = upfront_create_region(array(
-	'name' => "main", 
+	'name' => "main",
 	'title' => __("Main Area"),
 	'scope' => "local",
 	'type' => $type,
@@ -20,20 +20,176 @@ $main = upfront_create_region(array(
 	'background_color' => '#c5d0db'
 ));
 
-$main->add_element('Uposts', array(	
-	'id' => 'default-posts',
-	'columns' => 24,
-	'rows' => 20,
-	'options' => array(
-		'content_type' => 'excerpt',
-		'featured_image' => '1',
-		'post_data' => array('date')
-	)
+$main->add_element("Posts", array (
+  'columns' => '24',
+  'margin_left' => '0',
+  'margin_right' => '0',
+  'margin_top' => '0',
+  'margin_bottom' => '0',
+  'class' => 'default-posts',
+  'id' => 'default-posts',
+  'options' =>
+  array (
+    'type' => 'PostsModel',
+    'view_class' => 'PostsView',
+    'has_settings' => 1,
+    'class' => 'c24 uposts-object',
+    'id_slug' => 'posts',
+    'display_type' => 'list',
+    'list_type' => 'generic',
+    'offset' => 1,
+    'taxonomy' => '',
+    'term' => '',
+    'content' => 'excerpt',
+    'limit' => 5,
+    'pagination' => '',
+    'sticky' => '',
+    'posts_list' => '',
+    'thumbnail_size' => 'large',
+    'custom_thumbnail_width' => 200,
+    'custom_thumbnail_height' => 200,
+    'post_parts' =>
+    array (
+      0 => 'date_posted',
+      1 => 'author',
+      2 => 'gravatar',
+      3 => 'comment_count',
+      4 => 'featured_image',
+      5 => 'title',
+      6 => 'content',
+      7 => 'read_more',
+      8 => 'tags',
+      9 => 'categories',
+    ),
+    'enabled_post_parts' =>
+    array (
+      0 => 'date_posted',
+      1 => 'author',
+      2 => 'gravatar',
+      3 => 'comment_count',
+      4 => 'featured_image',
+      5 => 'title',
+      6 => 'content',
+      7 => 'read_more',
+      8 => 'tags',
+      9 => 'categories',
+    ),
+    'default_parts' =>
+    array (
+      0 => 'date_posted',
+      1 => 'author',
+      2 => 'gravatar',
+      3 => 'comment_count',
+      4 => 'featured_image',
+      5 => 'title',
+      6 => 'content',
+      7 => 'read_more',
+      8 => 'tags',
+      9 => 'categories',
+      10 => 'meta',
+    ),
+    'date_posted_format' => 'F j, Y g:i a',
+    'categories_limit' => 3,
+    'tags_limit' => 3,
+    'comment_count_hide' => 0,
+    'content_length' => 0,
+    'resize_featured' => '1',
+    'gravatar_size' => 200,
+    'preset' => 'default',
+    'post-part-date_posted' => '<div class="uposts-part date_posted">
+	Posted on <span class="datetime">{{datetime}}</span></div>',
+    'post-part-author' => '<div class="uposts-part author">
+	By <a href="{{url}}">{{name}}</a></div>',
+    'post-part-gravatar' => '<div class="uposts-part gravatar">
+	{{gravatar}}
+</div>',
+    'post-part-comment_count' => '<div class="uposts-part comment_count">
+	{{comment_count||No comments}}
+</div>',
+    'post-part-featured_image' => '<div class="uposts-part thumbnail" data-resize="{{resize}}">
+	{{thumbnail}}
+</div>',
+    'post-part-title' => '<div class="uposts-part title">
+	<h3><a href="{{permalink}}" title="{{title}}">{{title}}</a></h3>
+</div>',
+    'post-part-content' => '<div class="uposts-part content {{content_type}}">
+	{{content}}
+</div>',
+    'post-part-read_more' => '<div class="uposts-part read_more">
+	<a href="{{permalink}}">Read more</a></div>',
+    'post-part-tags' => '<div class="uposts-part post_tags">
+	{{tags}}
+</div>',
+    'post-part-categories' => '<div class="uposts-part post_categories">
+	{{categories}}
+</div>',
+    'post-part-meta' => '<div class="uposts-part meta">
+
+</div>
+',
+    'content_type' => 'excerpt',
+    'featured_image' => '1',
+    'post_data' =>
+    array (
+      0 => 'date',
+    ),
+    'element_id' => 'default-posts-object',
+    'padding_slider' => 15,
+    'top_padding_num' => 15,
+    'bottom_padding_num' => 15,
+    'use_padding' => 'yes',
+    'lock_padding' => 0,
+    'padding_number' => 15,
+    'left_padding_num' => 15,
+    'right_padding_num' => 15,
+    'anchor' => '',
+    'current_preset' => 'default',
+  ),
+  'row' => 20,
+  'sticky' => false,
+  'default_hide' => 0,
+  'hide' => 0,
+  'toggle_hide' => 1,
+  'wrapper_id' => 'default-posts-wrapper',
+  'wrapper_breakpoint' =>
+  array (
+    'tablet' =>
+    array (
+      'col' => 12,
+      'clear' => true,
+      'order' => 1,
+    ),
+    'mobile' =>
+    array (
+      'col' => 7,
+      'clear' => true,
+      'order' => 1,
+    ),
+    'current_property' =>
+    array (
+      0 => 'order',
+    ),
+  ),
+  'breakpoint' =>
+  array (
+    'tablet' =>
+    array (
+      'col' => 12,
+    ),
+    'current_property' =>
+    array (
+      0 => 'col',
+    ),
+    'mobile' =>
+    array (
+      'col' => 7,
+    ),
+  ),
 ));
 
 if ( $left_sidebar ){
 	$left = upfront_create_region(array(
-		'name' => "left-sidebar", 
+		'name' => "left-sidebar",
 		'title' => __("Left Sidebar"),
 		'scope' => "local",
 	), array(
@@ -41,8 +197,8 @@ if ( $left_sidebar ){
 		'background_type' => 'color',
 		'background_color' => '#fff'
 	));
-	
-	$left->add_element('PlainTxt', array(	
+
+	$left->add_element('PlainTxt', array(
 		'id' => 'default-left-text',
 		'columns' => 4,
 		'rows' => 50,
@@ -54,13 +210,13 @@ if ( $left_sidebar ){
 			"class" => "c24 upfront-plain_txt",
 		)
 	));
-	
+
 	$main->add_side_region($left, 'left');
 }
 
 if ( $right_sidebar ){
 	$right = upfront_create_region(array(
-		'name' => "right-sidebar", 
+		'name' => "right-sidebar",
 		'title' => __("Right Sidebar"),
 		'scope' => "local",
 	), array(
@@ -68,8 +224,8 @@ if ( $right_sidebar ){
 		'background_type' => 'color',
 		'background_color' => '#fff'
 	));
-	
-	$right->add_element('PlainTxt', array(	
+
+	$right->add_element('PlainTxt', array(
 		'id' => 'default-right-text',
 		'columns' => 4,
 		'rows' => 50,
@@ -81,9 +237,8 @@ if ( $right_sidebar ){
 			"class" => "c24 upfront-plain_txt",
 		)
 	));
-	
+
 	$main->add_side_region($right, 'right');
 }
 
 $regions->add($main);
-
