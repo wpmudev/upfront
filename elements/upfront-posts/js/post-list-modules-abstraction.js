c716ca6fbb61ef7ffe45fa0660f4dbee9f3527cb
Cvetan <cvetan@incsub.com>
2016-09-02 17:25:40 +0300

    Fix showing settings panel always on correct place and hide on scroll

--- elements/upfront-posts/js/post-list-modules-abstraction.js
+++ elements/upfront-posts/js/post-list-modules-abstraction.js
@@ -246,8 +246,34 @@
 				// Add shadow overlay
 				this.$el.find(".upfront-settings-item-title").addClass('active-panel');
 				
+				var me = this;
+                _.delay(function() { // Delay because opening animation causes wrong outerHeight results
+                    var in_sidebar = me.$el.parents('#sidebar-ui').length,
+                        in_settings = me.$el.parents('#element-settings-sidebar').length,
+                        settingsTitleHeight = 68;
+
+                    // Apply if select field is in sidebar or settings sidebar
+                    if(in_sidebar == 1 || in_settings == 1) {
+                        var setting_dropdown = me.$el.find('.upfront-settings-post-wrapper'),
+                            select = setting_dropdown.parent(),
+                            dropDownTop = select.offset().top - $('#element-settings-sidebar').offset().top,
+							dropDownTop = dropDownTop + settingsTitleHeight
+						;
+						
+						setting_dropdown.css("width", select.width() + 10);
+                        setting_dropdown.css('top', dropDownTop + "px");
+                        setting_dropdown.css('display', 'block');
+                    }
+                }, 10);
+				
+				$('.sidebar-panel-content, #sidebar-scroll-wrapper').on('scroll', this, this.on_scroll);
+				
 				return $content.show();
 			},
+			on_scroll: function(e) {
+				$('.upfront-settings-item-title').removeClass('active-panel');
+				$('.upfront-settings-post-wrapper').hide();
+            },
 			hide_all: function() {
 				var $wrapper = this.$el.parent();
 				$wrapper.parent().find(".upfront-settings-item-title").removeClass('active-panel');
