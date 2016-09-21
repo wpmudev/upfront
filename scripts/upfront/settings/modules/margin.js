48696b663f820a57cadc8e8d8807277e19a0e358
Cvetan <cvetan@incsub.com>
2016-09-21 13:31:40 +0300

    Fix margin module to follow new padding behavior

--- scripts/upfront/settings/modules/margin.js
+++ scripts/upfront/settings/modules/margin.js
@@ -30,22 +30,21 @@ define([
 						//Toggle margin fields
 						if(value == "yes") {
 							if(lock == "yes") {
-								stateSettings.find('.margin-slider').show();
-								stateSettings.find('.margin-number').show();
-							} else {
-								stateSettings.find('.margin-top').show();
-								stateSettings.find('.margin-bottom').show();
-								stateSettings.find('.margin-left').show();
-								stateSettings.find('.margin-right').show();
+								stateSettings.find('.margin-left').find('input').prop( "disabled", true ).css('opacity', 0.4);
+								stateSettings.find('.margin-bottom').find('input').prop( "disabled", true ).css('opacity', 0.4);
+								stateSettings.find('.margin-right').find('input').prop( "disabled", true ).css('opacity', 0.4);
 							}
+							
+							stateSettings.find('.margin-top').show();
+							stateSettings.find('.margin-bottom').show();
+							stateSettings.find('.margin-left').show();
+							stateSettings.find('.margin-right').show();
 							stateSettings.find('.margin-lock').show();
 						} else {
 							stateSettings.find('.margin-top').hide();
 							stateSettings.find('.margin-bottom').hide();
 							stateSettings.find('.margin-left').hide();
 							stateSettings.find('.margin-right').hide();
-							stateSettings.find('.margin-slider').hide();
-							stateSettings.find('.margin-number').hide();
 							stateSettings.find('.margin-lock').hide();
 						}
 					}
@@ -68,12 +67,9 @@ define([
 
 						//Toggle border radius fields
 						if(value == "yes" && usemargin == "yes") {
-							stateSettings.find('.margin-slider').show();
-							stateSettings.find('.margin-number').show();
-							stateSettings.find('.margin-top').hide();
-							stateSettings.find('.margin-bottom').hide();
-							stateSettings.find('.margin-left').hide();
-							stateSettings.find('.margin-right').hide();
+							stateSettings.find('.margin-left').find('input').prop( "disabled", true ).css('opacity', 0.4);
+							stateSettings.find('.margin-bottom').find('input').prop( "disabled", true ).css('opacity', 0.4);
+							stateSettings.find('.margin-right').find('input').prop( "disabled", true ).css('opacity', 0.4);
 
 							me.model.set(me.options.fields.left_num, margin);
 							me.model.set(me.options.fields.top_num, margin);
@@ -83,91 +79,15 @@ define([
 							margin_top.get_field().val(margin);
 							margin_right.get_field().val(margin);
 							margin_bottom.get_field().val(margin);
+							
 						} else {
-							if(usemargin == "yes") {
-								stateSettings.find('.margin-slider').hide();
-								stateSettings.find('.margin-number').hide();
-								stateSettings.find('.margin-top').show();
-								stateSettings.find('.margin-bottom').show();
-								stateSettings.find('.margin-left').show();
-								stateSettings.find('.margin-right').show();
-							}
+							stateSettings.find('.margin-left').find('input').prop( "disabled", false ).css('opacity', 1);
+							stateSettings.find('.margin-bottom').find('input').prop( "disabled", false ).css('opacity', 1);
+							stateSettings.find('.margin-right').find('input').prop( "disabled", false ).css('opacity', 1);
 						}
 					},
 					change: function(value) {
 						me.model.set(me.options.fields.lock, value);
-					}
-				}),
-
-
-				locked_slider = new Upfront.Views.Editor.Field.Slider({
-					className: 'margin-slider upfront-field-wrap',
-					model: this.model,
-					name: me.options.fields.slider,
-					suffix: l10n.px,
-					step: 5,
-					min: 0,
-					max: 250,
-					change: function (value) {
-						//Update all margin values
-						me.model.set(me.options.fields.slider, value);
-						me.model.set(me.options.fields.margin_number, value, true);
-						me.model.set(me.options.fields.left_num, value, true);
-						me.model.set(me.options.fields.top_num, value, true);
-						me.model.set(me.options.fields.right_num, value, true);
-						me.model.set(me.options.fields.bottom_num, value, true);
-
-						locked_num.get_field().val(value);
-						margin_left.get_field().val(value);
-						margin_top.get_field().val(value);
-						margin_right.get_field().val(value);
-						margin_bottom.get_field().val(value);
-
-						//Enable margin fields
-						me.enable_lock_margin();
-					},
-					show: function() {
-						var value = me.model.get(me.options.fields.margin_number);
-						if(value > 250) {
-							me.$el.find('.margin-slider').css('opacity', 0.6);
-						} else {
-							me.$el.find('.margin-slider').css('opacity', 1);
-						}
-					}
-				}),
-
-				locked_num = new Upfront.Views.Editor.Field.Number({
-					className: 'margin-number',
-					model: this.model,
-					name: me.options.fields.margin_number,
-					label: '',
-					step: 5,
-					min: 0,
-					change: function(value) {
-						// me.model.set('margin_number', value);
-						me.model.set(me.options.fields.slider, value);
-						me.model.set(me.options.fields.margin_number, value, true);
-						locked_slider.$el.find('#'+locked_slider.get_field_id()).slider('value', value);
-
-						//Update all margin values
-						me.model.set(me.options.fields.left_num, value, true);
-						me.model.set(me.options.fields.top_num, value, true);
-						me.model.set(me.options.fields.right_num, value, true);
-						me.model.set(me.options.fields.bottom_num, value, true);
-						margin_left.get_field().val(value);
-						margin_top.get_field().val(value);
-						margin_right.get_field().val(value);
-						margin_bottom.get_field().val(value);
-
-						//Enable margin fields
-						me.enable_lock_margin();
-
-						//Lower opacity if value is bigger than the slider MAX_VALUE
-						if(value > 250) {
-							me.$el.find('.margin-slider').css('opacity', 0.6);
-						} else {
-							me.$el.find('.margin-slider').css('opacity', 1);
-						}
 					}
 				}),
 
