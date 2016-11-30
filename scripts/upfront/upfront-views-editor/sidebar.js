d74750ce4d0365f2c03c9b9a88e433a4e70e378e
diobeth <diobethrayvilla@gmail.com>
2016-11-30 12:03:01 +0800

    Fixes for: [Theme Colors, Group BG Colors, Theme Typography Color] Color ickers are hidden inside th sidebar... Sidebar is cliping them. > https://www.meistertask.com/app/task/dgIpzppW

--- scripts/upfront/upfront-views-editor/sidebar.js
+++ scripts/upfront/upfront-views-editor/sidebar.js
@@ -12,7 +12,8 @@
         'scripts/upfront/upfront-views-editor/sidebar/commands/sidebar-commands-primary-post-type',
         'scripts/upfront/upfront-views-editor/breakpoint',
         'scripts/upfront/upfront-views-editor/sidebar/sidebar-panel-responsive-section-typography',
-        'scripts/upfront/upfront-views-editor/commands/command-save-post'
+        'scripts/upfront/upfront-views-editor/commands/command-save-post',
+        'scripts/perfect-scrollbar/perfect-scrollbar'
     ], function (
         SidebarPanel,
         DraggableElement,
@@ -22,7 +23,8 @@
         SidebarCommands_PrimaryPostType,
         Breakpoint,
         SidebarPanel_Responsive_Settings_Section_Typography,
-				CommandSavePost
+		CommandSavePost,
+		perfectScrollbar
     ) {
         var SidebarCommands_Control = Commands.Commands.extend({
             className: function() {
@@ -140,6 +142,24 @@
 
                 this.$el.html(this.template);
                 this.$el.find('.sidebar-panel-content').html(typography_section.el);
+				
+				var $sidebar_panel_content = this.$el.find('.sidebar-panel-content');
+				// Add JS Scrollbar.
+				perfectScrollbar.initialize($sidebar_panel_content[0], {
+					suppressScrollX: true
+				});
+				
+				var me = this;
+				// When color spectrum is shown, set positions
+				Upfront.Events.on("color:spectrum:show", function() {
+					$sidebar_panel_content.css('position', 'static');
+					$sidebar_panel_content.closest('li.sidebar-panel-settings').css('position', 'relative');
+				});
+				// When color spectrum is hidden, reset positions
+				Upfront.Events.on("color:spectrum:hide", function() {
+					$sidebar_panel_content.css('position', 'relative');
+					$sidebar_panel_content.closest('li.sidebar-panel-settings').css('position', 'static');
+				});
             }
         });
 
