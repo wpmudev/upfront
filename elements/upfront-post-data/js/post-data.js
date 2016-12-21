d4f2e04e77e035d330f79502debf06a37e8d500e
Daniel <sheparddw@gmail.com>
2016-12-21 10:31:33 -0800

    Add multi-element class to post data element on resize/drop.

--- elements/upfront-post-data/js/post-data.js
+++ elements/upfront-post-data/js/post-data.js
@@ -86,6 +86,9 @@ var PostDataPartView = Upfront.Views.ObjectView.extend({
 
 	on_element_drop: function () {
 		this.update_height();
+		// Add/remove multiple module class.
+		$object = this.$el.find('.upfront-editable_entity:first');
+		this.add_multiple_module_class($object);
 	},
 
 	render_view: function (markup) {
@@ -1057,6 +1060,11 @@ var PostDataView = Upfront.Views.ObjectGroup.extend({
 
 			// Save image crop from resize
 			this.saveTemporaryResizing(elementSize);
+
+			// Add/remove multiple module class.
+			$object = this.$el.find('.upfront-editable_entity:first');
+			this.add_multiple_module_class($object);
+
 		}
 
 		// Also resize child objects if it's only one object
