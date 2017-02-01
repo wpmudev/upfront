ad84abee7d818011892173e6bba6add9bf35b832
Daniel <sheparddw@gmail.com>
2017-01-31 18:52:20 -0800

    Remove Edit Profile link per spec and adjust Sidebar header more menu button styling.

--- scripts/upfront/upfront-views-editor/sidebar/sidebar-profile.js
+++ scripts/upfront/upfront-views-editor/sidebar/sidebar-profile.js
@@ -16,8 +16,7 @@
                     tpl
                     ;
                 tpl = '<div class="sidebar-profile-avatar"><img src="//www.gravatar.com/avatar/{{ gravatar ? gravatar : "gravatar" }}?s=25" /></div>' +
-                    '<div class="sidebar-profile-detail"><span class="sidebar-profile-name">{{name}}</span><span class="sidebar-profile-role">{{role}}</span></div>' +
-                    (roles.length ? '<div class="sidebar-profile-edit"><a class="upfront-icon upfront-icon-edit" data-bypass="true" title="'+  l10n.edit_profile +'" href="{{edit_url}}">' + l10n.edit_profile + '</a></div>' : '');
+                    '<div class="sidebar-profile-detail"><span class="sidebar-profile-name">{{name}}</span><span class="sidebar-profile-role">{{role}}</span></div>';
                 this.$el.html(_.template(tpl,
                     {
                         gravatar: data.gravatar,
