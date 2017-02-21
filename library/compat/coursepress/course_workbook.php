be565638e6726bcc19138b32b8653356bb2ba086
Ivan Ivanić <spoygg@spoygg.com>
2017-02-21 08:51:09 +0100

    Improving CP sample content

--- library/compat/coursepress/course_workbook.php
+++ library/compat/coursepress/course_workbook.php
@@ -1,38 +1,73 @@
 <div class="submenu-main-container cp-submenu">
-<ul id="submenu-main" class="submenu nav-submenu">
-<li class="submenu-item submenu-units "><a href="http://local.wordpressmu.dev/cp-compat/courses/course-number-two/units/" class="course-units-link">Units</a></li>
-<li class="submenu-item submenu-notifications "><a href="http://local.wordpressmu.dev/cp-compat/courses/course-number-two/notifications">Notifications</a></li>
-<li class="submenu-item submenu-discussions "><a href="http://local.wordpressmu.dev/cp-compat/courses/course-number-two/discussion">Discussions</a></li>
-<li class="submenu-item submenu-workbook submenu-active"><a href="http://local.wordpressmu.dev/cp-compat/courses/course-number-two/workbook">Workbook</a></li>
-<li class="submenu-item submenu-info"><a href="http://local.wordpressmu.dev/cp-compat/courses/course-number-two/">Course Details</a></li>
-<li class="submenu-item submenu-certificate "><a href="http://local.wordpressmu.dev/cp-compat?fdcpf=http://local.wordpressmu.dev/cp-compat/wp-content/uploads/sites/3/pdf-cache/4c/c7/70da04859cd099aad8af36aa5bba.pdf&amp;c=122&amp;u=1">Certificate</a></li>
-</ul></div>
-<div class="cp-student-workbook">
-<h3 class="course-completion-progress">Course completion: <small>100%</small></h3>
-<div class="workbook">
-<table class="workbook-table">
-<tbody><tr class="row-unit">
-<th colspan="2" class="workbook-unit unit-123">First unit</th>
-<th class="td-right">Progress: 100%</th>
-</tr>
-<tr class="row-module">
-<td class="column-title">Here be answering questions</td>
-<td class="column-answer">
-<p class="answer chosen-correct">Answer A sdfsd</p>
-</td>
-<td class="td-right">100%</td>
-</tr>
-<tr class="row-unit">
-<th colspan="2" class="workbook-unit unit-132">Untitled Unit 2nd</th>
-<th class="td-right">Progress: 100%</th>
-</tr>
-<tr class="row-module">
-<td class="column-title">some sdlfjsdljkf</td>
-<td class="column-answer">
-<p class="answer chosen-correct">Answer A</p>
-</td>
-<td class="td-right">100%</td>
-</tr>
-</tbody></table>
+	<ul id="submenu-main" class="submenu nav-submenu">
+		<li class="submenu-item submenu-units "><a href="#" class="course-units-link">Units</a></li>
+		<li class="submenu-item submenu-notifications "><a href="#">Notifications</a></li>
+		<li class="submenu-item submenu-discussions "><a href="#">Discussions</a></li>
+		<li class="submenu-item submenu-workbook submenu-active"><a href="#">Workbook</a></li>
+		<li class="submenu-item submenu-info"><a href="#">Course Details</a></li>
+	</ul>
 </div>
+<div class="cp-student-workbook">
+	<h3 class="course-completion-progress">Course completion: <small>53%</small></h3>
+	<div class="workbook">
+		<table class="workbook-table">
+			<tbody>
+				<tr class="row-unit">
+					<th colspan="2" class="workbook-unit unit-469">Intro</th>
+					<th class="td-right">Progress: 100%</th>
+				</tr>
+				<tr>
+					<td colspan="3" class="non-gradable">No gradable modules under this unit.</td>
+				</tr>
+				<tr class="row-unit">
+					<th colspan="2" class="workbook-unit unit-377">First Unit: Getting Started</th>
+					<th class="td-right">Progress: 59%</th>
+				</tr>
+				<tr class="row-module">
+					<td class="column-title">This is multiple choice module</td>
+					<td class="column-answer">
+						<p class="answer chosen-correct">Answer A</p>
+					</td>
+					<td class="td-right">0%</td>
+				</tr>
+				<tr class="row-module">
+					<td class="column-title">This is single choice module</td>
+					<td class="column-answer">
+						<p class="answer chosen-correct">Answer A</p>
+					</td>
+					<td class="td-right">0%</td>
+				</tr>
+				<tr class="row-module">
+					<td class="column-title">This is selectable module</td>
+					<td class="column-answer">
+						<p class="answer chosen-correct">Answer A</p>
+					</td>
+					<td class="td-right">0%</td>
+				</tr>
+				<tr class="row-module">
+					<td class="column-title">This is short answer module</td>
+					<td class="column-answer">No answer!</td>
+					<td class="td-right">0%</td>
+				</tr>
+				<tr class="row-module">
+					<td class="column-title">This is long answer module</td>
+					<td class="column-answer">No answer!</td>
+					<td class="td-right">0%</td>
+				</tr>
+				<tr class="row-module">
+					<td class="column-title">This is quiz module</td>
+					<td class="column-answer">
+						<p class="question">Multiple choice question?</p>
+						<p class="question">Single choice question?</p>
+					</td>
+					<td class="td-right">0%</td>
+				</tr>
+				<tr class="row-module">
+					<td class="column-title">This is form module</td>
+					<td class="column-answer"></td>
+					<td class="td-right">0%</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
 </div>
