00395b91173d160c6bb5b9c0f550352fa1147899
Ivan Ivanić <spoygg@spoygg.com>
2017-02-23 16:18:43 +0100

    Refactor CoursePress compat class

--- library/compat/class_upfront_compat_coursepress.php
+++ library/compat/class_upfront_compat_coursepress.php
@@ -2,6 +2,79 @@
 
 class Upfront_Compat_CoursePress {
 
+	private $cp_layouts = array(
+		'course'                        => array(
+			'display_name' => 'CoursePress Course',
+			'type' => 'single',
+			'title' => 'Course Number One',
+		),
+		'course_archive'                => array(
+			'display_name' => 'CoursePress All Courses',
+			'type' => 'single',
+			'title' => 'All Courses',
+		),
+		'unit_archive'                  => array(
+			'display_name' => 'CoursePress Course Units',
+			'type' => 'single',
+			'title' => 'Course Number One',
+		),
+		'unit'                          => array(
+			'display_name' => 'CoursePress Course Unit',
+			'type' => 'single',
+			'title' => 'Course Number One',
+		),
+		'course_notifications_archive'  => array(
+			'display_name' => 'CoursePress Course Notifications',
+			'type' => 'single',
+			'title' => 'Course Number One',
+		),
+		'course_discussion_archive'     => array(
+			'display_name' => 'CoursePress Course All Discussions',
+			'type' => 'single',
+			'title' => 'Course Number One',
+		),
+		'course_discussion'             => array(
+			'display_name' => 'CoursePress Course Discussion',
+			'type' => 'single',
+			'title' => 'Course Number One',
+		),
+		'course_workbook'               => array(
+			'display_name' => 'CoursePress Course Workbook',
+			'type' => 'single',
+			'title' => 'Course Number One',
+		),
+		'course_grades_archive'         => array(
+			'display_name' => 'CoursePress Course Grades',
+			'type' => 'single',
+			'title' => 'Course Number One',
+		),
+		'coursepress_student_login'     => array(
+			'display_name' => 'CoursePress Student Login',
+			'type' => 'single',
+			'title' => 'Course Number One',
+		),
+		'coursepress_student_signup'    => array(
+			'display_name' => 'CoursePress Student Signup',
+			'type' => 'single',
+			'title' => 'Course Number One',
+		),
+		'coursepress_student_dashboard' => array(
+			'display_name' => 'CoursePress Courses Dashboard',
+			'type' => 'single',
+			'title' => 'Course Number One',
+		),
+		'coursepress_student_settings'  => array(
+			'display_name' => 'CoursePress Student Settings',
+			'type' => 'single',
+			'title' => 'Course Number One',
+		),
+		'coursepress_instructor'  => array(
+			'display_name' => 'CoursePress Instructor',
+			'type' => 'single',
+			'title' => 'Instructor A',
+		),
+	);
+
 	public function __construct() {
 		$this->add_hooks();
 	}
@@ -12,7 +85,6 @@ class Upfront_Compat_CoursePress {
 		// Force always loading CP styles in builder
 		if (upfront_exporter_is_running() && class_exists('CoursePress_Core')) CoursePress_Core::$is_cp_page = true;
 
-		add_filter('upfront_pre_get_post_markup_use_post', array($this, 'use_post'), 10, 3);
 		add_filter('upfront-plugins_layouts', array($this, 'add_plugins_layouts'));
 		add_filter('upfront-forbidden_post_data_types', array($this, 'forbidden_post_data_types'));
 		add_filter('upfront-layout_to_name', array($this, 'layout_to_name'), 10, 4);
@@ -22,26 +94,15 @@ class Upfront_Compat_CoursePress {
 		add_filter('upfront-post_data-get_content-after', array($this, 'wrap_with_coursepress_css_class'), 99);
 	}
 
+	/**
+	 * Wrap CoursePress content with css class for easier styling.
+	 */
 	public function wrap_with_coursepress_css_class($content) {
 		$layout = Upfront_Layout::get_parsed_cascade();
-		$cp_layouts = array(
-			'single-course',
-			'single-course_archive',
-			'single-course_discussion',
-			'single-course_discussion_archive',
-			'single-course_grades_archive',
-			'single-course_notifications_archive',
-			'single-course_workbook',
-			'single-coursepress_student_dashboard',
-			'single-coursepress_student_login',
-			'single-coursepress_student_settings',
-			'single-coursepress_student_signup',
-			'single-unit',
-			'single-unit_archive',
-			'single-coursepress_instructor'
-		);
 
-		if (in_array($layout['item'], $cp_layouts) === false) return $content;
+		$i = str_replace('single-', '', $layout['item']);
+
+		if (empty($this->cp_layouts[$i])) return $content;
 
 		return '<div class="coursepress-content">' . $content . '</div>';
 	}
@@ -82,6 +143,7 @@ class Upfront_Compat_CoursePress {
 
 		return '';
 	}
+
 	/**
 	 * Checks against post object if current page is CoursePress page.
 	 *
@@ -89,36 +151,16 @@ class Upfront_Compat_CoursePress {
 	 *
 	 * @return bool
 	 */
-	public static function is_coursepress_page($post) {
-		$cp_post_types = array(
-			'course_notifications_archive',
-			'course_discussion_archive',
-			'coursepress_student_dashboard',
-			'coursepress_student_signup',
-			'coursepress_student_login',
-			'unit',
-			'course',
-			'course_grades_archive',
-			'course_archive',
-			'course_workbook',
-			'coursepress_instructor'
-		);
+	public function is_coursepress_page($post) {
+		if (empty($this->cp_layouts[$post->post_type])) return false;
 
-		if (in_array($post->post_type, $cp_post_types)) return true;
-
-		return false;
+		return true;
 	}
 
 	/**
-	 * Filter to determine if current page is CoursePress page.
+	 * Force only relevant post data parts in plugin pages. Allow only
+	 * title and content.
 	 */
-	public function use_post($use, $post, $properties) {
-		if (self::is_coursepress_page($post)) {
-			return true;
-		}
-		return $use;
-	}
-
 	public function forbidden_post_data_types($types) {
 		$post = get_post();
 		if (is_null($post)) return $types;
@@ -135,294 +177,64 @@ class Upfront_Compat_CoursePress {
 		return  ob_get_clean();
 	}
 
-	// List WC layouts to match againts current layout in editor
+
+	/**
+	 * List CoursePress layouts to match againts current layout in editor and builder.
+	 * This allows editor/builder to hide settings that do not apply and provides sample
+	 * content.
+	 */
 	function add_plugins_layouts($layouts) {
-		$sampleContents = array(
-			'course' => $this->get_sample_content('course'),
-			'course_archive' => $this->get_sample_content('course_archive'),
-			'unit_archive' => $this->get_sample_content('unit_archive'),
-			'unit' => $this->get_sample_content('unit'),
-			'course_notifications_archive' => $this->get_sample_content('course_notifications_archive'),
-			'course_discussion_archive' => $this->get_sample_content('course_discussion_archive'),
-			'course_discussion' => $this->get_sample_content('course_discussion'),
-			'course_workbook' => $this->get_sample_content('course_workbook'),
-			'course_grades_archive' => $this->get_sample_content('course_grades_archive'),
-			'coursepress_student_login' => $this->get_sample_content('coursepress_student_login'),
-			'coursepress_student_signup' => $this->get_sample_content('coursepress_student_signup'),
-			'coursepress_student_dashboard' => $this->get_sample_content('coursepress_student_dashboard'),
-			'coursepress_student_settings' => $this->get_sample_content('coursepress_student_settings'),
-		);
+		$sampleContents = array();
+		$cpLayouts = array();
+
+		foreach ($this->cp_layouts as $item=>$info) {
+
+			$sampleContents[$item] = $this->get_sample_content($item);
+
+			$cpLayouts[] = array(
+				'item' => 'single-' . $item,
+				'specificity' => 'single-' . $item,
+				'type' => $info['type'],
+				'content' => $item,
+				'title' => __($info['title'], 'upfront'),
+				'display_name' => __($info['display_name'], 'upfront'),
+				'killPostSettings' => __('This is virtual page handled by CoursePress.', 'upfront'),
+			);
+		}
 
 		$layouts['course-press'] = array(
 			'pluginName' => 'CoursePress',
 			'sampleContents' => $sampleContents,
-			'layouts' => array(
-				array(
-					'item' => 'single-course',
-					'specificity' => 'single-course',
-					'type' => 'single',
-					'content' => 'course',
-					'title' => __('Course Number One', 'upfront'),
-					'displayname' => __('CoursePress Course page', 'upfront'),
-					'killPostSettings' => __('This is virtual page handled by CoursePress.', 'upfront'),
-				),
-				array(
-					'item' => 'single-course_archive',
-					'specificity' => 'single-course_archive',
-					'type' => 'single',
-					'content' => 'course_archive',
-					'title' => __('All Courses', 'upfront'),
-					'displayname' => __('CoursePress Course Archive page', 'upfront'),
-					'killPostSettings' => __('This is virtual page handled by CoursePress.', 'upfront'),
-				),
-				array(
-					'item' => 'single-unit_archive',
-					'specificity' => 'single-unit_archive',
-					'type' => 'single',
-					'content' => 'unit_archive',
-					'title' => __('Course Number One', 'upfront'),
-					'displayname' => __('CoursePress Course Units page', 'upfront'),
-					'killPostSettings' => __('This is virtual page handled by CoursePress.', 'upfront'),
-				),
-				array(
-					'item' => 'single-unit',
-					'specificity' => 'single-unit',
-					'type' => 'single',
-					'content' => 'unit',
-					'title' => __('Course Number One', 'upfront'),
-					'displayname' => __('CoursePress Course Unit page', 'upfront'),
-					'killPostSettings' => __('This is virtual page handled by CoursePress.', 'upfront'),
-				),
-				array(
-					'item' => 'single-course_notifications_archive',
-					'specificity' => 'single-course_notifications_archive',
-					'type' => 'single',
-					'content' => 'course_notifications_archive',
-					'title' => __('Course Number One', 'upfront'),
-					'displayname' => __('CoursePress Course Notifications page', 'upfront'),
-					'killPostSettings' => __('This is virtual page handled by CoursePress.', 'upfront'),
-				),
-				array(
-					'item' => 'single-course_discussion_archive',
-					'specificity' => 'single-course_discussion_archive',
-					'type' => 'single',
-					'content' => 'course_discussion_archive',
-					'title' => __('Course Number One', 'upfront'),
-					'displayname' => __('CoursePress Course Discussions page', 'upfront'),
-					'killPostSettings' => __('This is virtual page handled by CoursePress.', 'upfront'),
-				),
-				array(
-					'item' => 'single-course_discussion',
-					'specificity' => 'single-course_discussion',
-					'type' => 'single',
-					'content' => 'course_discussion',
-					'title' => __('Course Number One', 'upfront'),
-					'displayname' => __('CoursePress Course Discussion page', 'upfront'),
-					'killPostSettings' => __('This is virtual page handled by CoursePress.', 'upfront'),
-				),
-				array(
-					'item' => 'single-course_workbook',
-					'specificity' => 'single-course_workbook',
-					'type' => 'single',
-					'content' => 'course_workbook',
-					'title' => __('Course Number One', 'upfront'),
-					'displayname' => __('CoursePress Course Workbook page', 'upfront'),
-					'killPostSettings' => __('This is virtual page handled by CoursePress.', 'upfront'),
-				),
-				array(
-					'item' => 'single-course_grades_archive',
-					'specificity' => 'single-course_grades_archive',
-					'type' => 'single',
-					'content' => 'course_grades_archive',
-					'title' => __('Course Number One', 'upfront'),
-					'displayname' => __('CoursePress Course Workbook page', 'upfront'),
-					'killPostSettings' => __('This is virtual page handled by CoursePress.', 'upfront'),
-				),
-				array(
-					'item' => 'single-coursepress_student_login',
-					'specificity' => 'single-coursepress_student_login',
-					'type' => 'single',
-					'content' => 'coursepress_student_login',
-					'title' => __('Course Number One', 'upfront'),
-					'displayname' => __('CoursePress Student Login page', 'upfront'),
-					'killPostSettings' => __('This is virtual page handled by CoursePress.', 'upfront'),
-				),
-				array(
-					'item' => 'single-coursepress_student_signup',
-					'specificity' => 'single-coursepress_student_signup',
-					'type' => 'single',
-					'content' => 'coursepress_student_signup',
-					'title' => __('Course Number One', 'upfront'),
-					'displayname' => __('CoursePress Student Signup page', 'upfront'),
-					'killPostSettings' => __('This is virtual page handled by CoursePress.', 'upfront'),
-				),
-				array(
-					'item' => 'single-coursepress_student_dashboard',
-					'specificity' => 'single-coursepress_student_dashboard',
-					'type' => 'single',
-					'content' => 'coursepress_student_dashboard',
-					'title' => __('Course Number One', 'upfront'),
-					'displayname' => __('CoursePress Courses Dashboard page', 'upfront'),
-					'killPostSettings' => __('This is virtual page handled by CoursePress.', 'upfront'),
-				),
-				array(
-					'item' => 'single-coursepress_student_settings',
-					'specificity' => 'single-coursepress_student_settings',
-					'type' => 'single',
-					'content' => 'coursepress_student_settings',
-					'title' => __('Course Number One', 'upfront'),
-					'displayname' => __('CoursePress Student Settings page', 'upfront'),
-					'killPostSettings' => __('This is virtual page handled by CoursePress.', 'upfront'),
-				),
-			)
+			'layouts' => $cpLayouts
 		);
 
 		return $layouts;
 	}
 
+	/**
+	 * Translate layout names for builder "Layouts" popup.
+	 */
 	public function layout_to_name($layout_name, $type, $item, $specificity) {
-		if ($specificity === 'single-course') {
-			return __('CoursePress Course', 'upfront');
-		}
-		if ($specificity === 'single-course_archive') {
-			return __('CoursePress All Courses', 'upfront');
-		}
-		if ($specificity === 'single-unit_archive') {
-			return __('CoursePress Course Units', 'upfront');
-		}
+		$s = str_replace('single-', '', $specificity);
 
-		if ($specificity === 'single-unit') {
-			return __('CoursePress Course Unit', 'upfront');
-		}
-
-		if ($specificity === 'single-course_notifications_archive') {
-			return __('CoursePress Course Notifications', 'upfront');
-		}
-
-		if ($specificity === 'single-course_discussion_archive') {
-			return __('CoursePress Course All Discussions', 'upfront');
-		}
-
-		if ($specificity === 'single-course_discussion') {
-			return __('CoursePress Course Discussion', 'upfront');
-		}
-
-		if ($specificity === 'single-course_workbook') {
-			return __('CoursePress Course Workbook', 'upfront');
-		}
-
-		if ($specificity === 'single-course_grades_archive') {
-			return __('CoursePress Course Grades', 'upfront');
-		}
-		if ($specificity === 'single-coursepress_student_login') {
-			return __('CoursePress Student Login', 'upfront');
-		}
-		if ($specificity === 'single-coursepress_student_signup') {
-			return __('CoursePress Student Signup', 'upfront');
-		}
-		if ($specificity === 'single-coursepress_student_dashboard') {
-			return __('CoursePress Courses Dashboard', 'upfront');
-		}
-		if ($specificity === 'single-coursepress_student_settings') {
-			return __('CoursePress Student Settings', 'upfront');
-		}
-
+		if (!empty($this->cp_layouts[$s])) return __($this->cp_layouts[$s]['display_name'], 'upfront');
 
 		return $layout_name;
 	}
 
+	/**
+	 * Add CoursePress layouts to builder "Layouts" popup.
+	 */
 	public function add_builder_available_layouts($layouts) {
-		$layouts[] = array(
-			'layout' => array(
-				'type' => 'single',
-				'item' => 'course',
-				'specificity' => 'single-course'
-			)
-		);
-		$layouts[] = array(
-			'layout' => array(
-				'type' => 'single',
-				'item' => 'course_archive',
-				'specificity' => 'single-course_archive'
-			)
-		);
-		$layouts[] = array(
-			'layout' => array(
-				'type' => 'single',
-				'item' => 'unit_archive',
-				'specificity' => 'single-unit_archive'
-			)
-		);
-		$layouts[] = array(
-			'layout' => array(
-				'type' => 'single',
-				'item' => 'unit',
-				'specificity' => 'single-unit'
-			)
-		);
-		$layouts[] = array(
-			'layout' => array(
-				'type' => 'single',
-				'item' => 'course_notifications_archive',
-				'specificity' => 'single-course_notifications_archive'
-			)
-		);
-		$layouts[] = array(
-			'layout' => array(
-				'type' => 'single',
-				'item' => 'course_discussion_archive',
-				'specificity' => 'single-course_discussion_archive'
-			)
-		);
-		$layouts[] = array(
-			'layout' => array(
-				'type' => 'single',
-				'item' => 'course_discussion',
-				'specificity' => 'single-course_discussion'
-			)
-		);
-		$layouts[] = array(
-			'layout' => array(
-				'type' => 'single',
-				'item' => 'course_workbook',
-				'specificity' => 'single-course_workbook'
-			)
-		);
-		$layouts[] = array(
-			'layout' => array(
-				'type' => 'single',
-				'item' => 'course_grades_archive',
-				'specificity' => 'single-course_grades_archive'
-			)
-		);
-		$layouts[] = array(
-			'layout' => array(
-				'type' => 'single',
-				'item' => 'coursepress_student_login',
-				'specificity' => 'single-coursepress_student_login'
-			)
-		);
-		$layouts[] = array(
-			'layout' => array(
-				'type' => 'single',
-				'item' => 'coursepress_student_signup',
-				'specificity' => 'single-coursepress_student_signup'
-			)
-		);
-		$layouts[] = array(
-			'layout' => array(
-				'type' => 'single',
-				'item' => 'coursepress_student_dashboard',
-				'specificity' => 'single-coursepress_student_dashboard'
-			)
-		);
-		$layouts[] = array(
-			'layout' => array(
-				'type' => 'single',
-				'item' => 'coursepress_student_settings',
-				'specificity' => 'single-coursepress_student_settings'
-			)
-		);
+		foreach( $this->cp_layouts as $item=>$info) {
+			$layouts[] = array(
+				'layout' => array(
+					'type' => $info['type'],
+					'item' => $item,
+					'specificity' => 'single-' . $item
+				)
+			);
+		}
 
 		return $layouts;
 	}
