aa74c30ab28a23eaccbeee92a5fc4d37f33bcddd
Ivan Ivanić <spoygg@spoygg.com>
2017-03-02 09:47:39 +0100

    Update youtube load on click per Ve's comments

--- elements/upfront-youtube/js/uyoutube-front.js
+++ elements/upfront-youtube/js/uyoutube-front.js
@@ -3,11 +3,16 @@
 	 * Replace main video placeholder image+button with actual main video.
 	 */
 	function replace_ph_with_video(ph) {
-		var video_id = ph.data('video-id');
+		if (ph.size() === 0) return;
+
+		var video_id = ph.data('video-id') || 'none';
 		var loop = ph.data('loop');
-		var width = ph.data('width');
-		var height = ph.data('height');
-		ph.after('<iframe src="https://www.youtube.com/embed/'+video_id+'?modestbranding=1&'+loop+'&autoplay=true" width="'+width+'" height="'+height+'"></iframe>');
+		var width = ph.data('width') || parseInt(ph.width(), 10);
+		var height = ph.data('height') || parseInt(ph.height(), 10);
+
+		if (loop) loop = '&' + loop;
+
+		ph.after('<iframe src="https://www.youtube.com/embed/'+video_id+'?modestbranding=1'+loop+'&autoplay=true" width="'+width+'" height="'+height+'"></iframe>');
 		ph.remove();
 	}
 
@@ -16,20 +21,21 @@
 	 * @param {Object} - jQuery object of selected list item
 	 */
 	function load_video_from_list(list_item) {
-		var pp = list_item.parent().prev();
+		var el = list_item.parents('.upfront-youtube-container');
+		var video_id = list_item.data('video-id') || 'none';
 
 		// Make sure iframe is present and initialized properly
-		if (pp.find('iframe').size() === 0) {
-			replace_ph_with_video(pp.find('.ufyt_main-video-placeholder'));
+		if (el.find('iframe').size() === 0) {
+			replace_ph_with_video(el.find('.ufyt_main-video-placeholder'));
 		}
 
 		// Setup ifram and title for selected video
-		var videoUrl =  'https://www.youtube.com/embed/' + list_item.data('video-id') + '?modestbranding=1';
-		pp.find('iframe').attr('src', videoUrl);
-		pp.find('h3').html(list_item.find('h4').html());
+		var videoUrl = 'https://www.youtube.com/embed/' + video_id + '?modestbranding=1';
+		el.find('iframe').attr('src', videoUrl);
+		el.find('.ufyt_main-video-title').html(list_item.find('h4').html());
 
 		// Handle hidding active video thumbnail
-		if(pp.parent().data('first-hidden')) {
+		if(el.data('first-hidden')) {
 			list_item.siblings().css('display', 'inline-block');
 			list_item.hide();
 		}
