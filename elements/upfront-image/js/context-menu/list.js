7afc44c37ce18e72d800225c0c33cc888fcc8b7b
Daniel Shepard <daniel.shepard@incsub.com>
2017-04-17 15:58:09 -0700

    Add Open Lightbox/Anchor/Link to context menu for image element.

--- elements/upfront-image/js/context-menu/list.js
+++ elements/upfront-image/js/context-menu/list.js
@@ -6,32 +6,53 @@ define(function() {
 			this.options = opts;
 			this.for_view = this.options.for_view;
 			var me = this;
-			var menuitemsarray = [
-						new Upfront.Views.ContextMenuItem({
-					get_label: function() {
-            if (me.for_view.isThemeImage()) {
-							return l10n.ctrl.edit_image;
-            }
-						if(me.for_view.$el.find('div.upfront-image-container > img').length > 0) {
-							return l10n.ctrl.edit_image;
-						} else {
-							return l10n.ctrl.add_image;
+			var menuitemsarray = [];
+			/* Open Link */
+			if (me.for_view.link.get('type') && (me.for_view.link.get('type') === 'external' ? me.for_view.link.get('url') !== "" : true)) {
+				menuitemsarray.push( new Upfront.Views.ContextMenuItem({
+						get_label: function() {
+							var linktype = me.for_view.link.get('type');
+							var content_l10n = Upfront.Settings.l10n.global.content;
+							if (linktype === 'lightbox')
+								return content_l10n.open_lightbox;
+							else if (linktype === 'anchor')
+								return content_l10n.scroll_to_anchor;
+							else if (linktype === 'entry')
+								return content_l10n.go_to_post;
+							else
+								return content_l10n.open_ext_link;
+						},
+						action: function() {
+							Upfront.Util.visitLink(me.for_view.link.get('url'));
 						}
-					},
-					action: function() {
-            if (me.for_view.isThemeImage()) {
-              me.for_view.replaceImage();
-              return;
-            }
-						if(me.for_view.$el.find('div.upfront-image-container > img').length > 0) {
-							me.for_view.editRequest();
-						} else {
-							me.for_view.openImageSelector();
-						}
+					}));
+			}
+			/* Edit/Add Image */
+			menuitemsarray.push( new Upfront.Views.ContextMenuItem({
+				get_label: function() {
+					if (me.for_view.isThemeImage()) {
+						return l10n.ctrl.edit_image;
 					}
-				})
-					];
+					if(me.for_view.$el.find('div.upfront-image-container > img').length > 0) {
+						return l10n.ctrl.edit_image;
+					} else {
+						return l10n.ctrl.add_image;
+					}
+				},
+				action: function() {
+					if (me.for_view.isThemeImage()) {
+						me.for_view.replaceImage();
+						return;
+					}
+					if(me.for_view.$el.find('div.upfront-image-container > img').length > 0) {
+						me.for_view.editRequest();
+					} else {
+						me.for_view.openImageSelector();
+					}
+				}
+			}));
 
+			/* Edit/Add Caption */
 			if(this.for_view.$el.find('div.upfront-image-container > img').length > 0) {
 				menuitemsarray.push( new Upfront.Views.ContextMenuItem({
 						get_label: function() {
