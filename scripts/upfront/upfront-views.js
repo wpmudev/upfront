98b38b41193586f3fc98548f8950d8eb5c708dc2
Daniel Shepard <daniel.shepard@incsub.com>
2017-04-17 10:52:08 -0700

    Fix issue with dragging new elements into regions giving console errors and not working.

--- scripts/upfront/upfront-views.js
+++ scripts/upfront/upfront-views.js
@@ -6764,8 +6764,7 @@ define([
 					};
 
 				this.$el.find('> .upfront-region-wrapper > .upfront-modules_container').css( {
-					width: Math.floor(css.width/grid.column_width) * grid.column_width,
-					'height': css.height
+					width: Math.floor(css.width/grid.column_width) * grid.column_width
 				});
 				this.$el.css(css);
 			},
