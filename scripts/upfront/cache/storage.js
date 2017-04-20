e18452ffdf34c1e5d23229c7747b5734d05325b1
vladislav <ve@incsub.com>
2017-04-20 05:54:57 +0200

    Boot cache storage event listening

--- scripts/upfront/cache/storage.js
+++ scripts/upfront/cache/storage.js
@@ -1,7 +1,32 @@
 ;define([
+	'scripts/upfront/cache/storage-stub',
 	'scripts/upfront/cache/storage-memory',
 	'scripts/upfront/cache/storage-persistent',
-], function (Memory, Persistent) {
+	'scripts/upfront/cache/storage-permanent',
+], function (Stub, Memory, Persistent, Permanent) {
 
-	return Persistent;
+	var implementation = Stub;
+
+	switch (
+		((Upfront || {}).mainData || {}).response_cache_level
+	) {
+
+		case 'memory':
+			implementation = Memory;
+			break;
+		case 'persistent':
+			implementation = Persistent;
+			break;
+		case 'permanent':
+			implementation = Permanent;
+			break;
+		default:
+			implementation = Stub;
+			break;
+	}
+
+	// Dispatch implementation event listening
+	if (!implementation.is_listening()) implementation.listen();
+
+	return implementation;
 });
